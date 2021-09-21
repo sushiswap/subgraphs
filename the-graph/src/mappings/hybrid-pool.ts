449c2dce27d1360d2e53a5d2c6b4f50602836f2b
LufyCZ <lufy@sushi.com>
2021-09-21 20:05:05 +0000

    feat: hybrid, index pools

---
+++ the-graph/src/mappings/hybrid-pool.ts
@@ -0,0 +1,77 @@
+import { Address, BigDecimal, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  Approval,
+  Burn,
+  Mint,
+  Swap,
+  Sync,
+  Transfer,
+} from "../../generated/templates/HybridPool/HybridPool";
+
+import { getOrCreateHybridPool } from "../functions";
+
+export function onMint(event: Mint): void {
+  log.debug("onMint...", []);
+
+  const pool = getOrCreateHybridPool(event.address);
+  pool.txCount = pool.txCount.plus(BigInt.fromI32(1));
+
+  pool.save();
+}
+
+export function onBurn(event: Burn): void {
+  log.debug("onBurn...", []);
+
+  const pool = getOrCreateHybridPool(event.address);
+  pool.txCount = pool.txCount.plus(BigInt.fromI32(1));
+
+  pool.save();
+}
+
+export function onSync(event: Sync): void {
+  log.debug("onSync...", []);
+
+  const pool = getOrCreateHybridPool(event.address);
+
+  pool.reserve0 = event.params.reserve0;
+  pool.reserve1 = event.params.reserve1;
+
+  pool.save();
+}
+
+export function onSwap(event: Swap): void {
+  log.debug("onSwap...", []);
+}
+
+export function onApproval(event: Approval): void {
+  log.debug("onApproval...", []);
+}
+
+export function onTransfer(event: Transfer): void {
+  log.debug("onTransfer... {} {} {}", [
+    event.params.amount.divDecimal(BigDecimal.fromString("1e18")).toString(),
+    event.params.recipient.toHex(),
+    event.params.sender.toHex(),
+  ]);
+
+  const pool = getOrCreateHybridPool(event.address);
+
+  // If sender is black hole, we're mintin'
+  if (
+    event.params.sender ==
+    Address.fromString("0x0000000000000000000000000000000000000000")
+  ) {
+    pool.totalSupply = pool.totalSupply.plus(event.params.amount);
+    pool.save();
+  }
+
+  // If recipient is black hole we're burnin'
+  if (
+    event.params.sender.toHex() == pool.id &&
+    event.params.recipient ==
+      Address.fromString("0x0000000000000000000000000000000000000000")
+  ) {
+    pool.totalSupply = pool.totalSupply.minus(event.params.amount);
+    pool.save();
+  }
+}
