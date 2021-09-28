e26805237efde2fec1f676d77ece556f63d0080c
Matthew Lilley <hello@matthewlilley.com>
2021-09-28 20:22:49 +0100

    refactor: token functions

--- the-graph/src/functions/token.ts
+++ the-graph/src/functions/token.ts
@@ -1,7 +1,6 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 
 import { ERC20 } from "../../generated/MasterDeployer/ERC20";
-import { NULL_CALL_RETURN_VALUE } from "../constants";
 import { NameBytes32 } from "../../generated/MasterDeployer/NameBytes32";
 import { SymbolBytes32 } from "../../generated/MasterDeployer/SymbolBytes32";
 import { Token } from "../../generated/schema";
@@ -9,69 +8,95 @@ import { Token } from "../../generated/schema";
 export function getOrCreateToken(id: Address): Token {
   let token = Token.load(id.toHex());
 
+  const decimals = getTokenDecimals(id);
+  const name = getTokenName(id);
+  const symbol = getTokenSymbol(id);
+
   if (token === null) {
     token = new Token(id.toHex());
-    token.name = getTokenName(id);
-    token.symbol = getTokenSymbol(id);
-    token.decimals = getTokenDecimals(id);
+    token.name = name.value;
+    token.nameSuccess = name.success;
+    token.symbol = symbol.value;
+    token.symbolSuccess = symbol.success;
+    token.decimals = decimals.value;
+    token.decimalsSuccess = decimals.success;
     token.save();
   }
 
   return token as Token;
 }
 
-export function getTokenSymbol(address: Address): string {
-  let erc20Contract = ERC20.bind(address);
+class Symbol {
+  success: boolean;
+  value: string;
+}
+
+export function getTokenSymbol(id: Address): Symbol {
+  let erc20Contract = ERC20.bind(id);
 
   const symbol = erc20Contract.try_symbol();
 
   if (!symbol.reverted) {
-    return symbol.value;
+    return { success: true, value: symbol.value.toString() };
   }
 
-  const symbolBytes32Contract = SymbolBytes32.bind(address);
+  const symbolBytes32Contract = SymbolBytes32.bind(id);
 
   const symbolBytes32 = symbolBytes32Contract.try_symbol();
 
   if (
     !symbolBytes32.reverted &&
-    symbolBytes32.value.toHex() != NULL_CALL_RETURN_VALUE
+    symbolBytes32.value.toHex() !=
+      "0x0000000000000000000000000000000000000000000000000000000000000001"
   ) {
-    return symbolBytes32.value.toString();
+    return { success: true, value: symbolBytes32.value.toString() };
   }
 
-  return "???";
+  return { success: false, value: "???" };
 }
 
-export function getTokenName(address: Address): string {
-  const erc20Contract = ERC20.bind(address);
+class Name {
+  success: boolean;
+  value: string;
+}
+
+export function getTokenName(id: Address): Name {
+  const erc20Contract = ERC20.bind(id);
 
   const name = erc20Contract.try_name();
 
   if (!name.reverted) {
-    return name.value;
+    return { success: true, value: name.value.toString() };
   }
 
-  const nameBytes32Contract = NameBytes32.bind(address);
+  const nameBytes32Contract = NameBytes32.bind(id);
 
   const nameBytes32 = nameBytes32Contract.try_name();
 
   if (
     !nameBytes32.reverted &&
-    nameBytes32.value.toHex() != NULL_CALL_RETURN_VALUE
+    nameBytes32.value.toHex() !=
+      "0x0000000000000000000000000000000000000000000000000000000000000001"
   ) {
-    return nameBytes32.value.toString();
+    return { success: true, value: nameBytes32.value.toString() };
   }
 
-  return "???";
+  return { success: false, value: "???" };
 }
 
-export function getTokenDecimals(address: Address): BigInt {
-  let contract = ERC20.bind(address);
-  let decimalValue = 18;
-  let decimalResult = contract.try_decimals();
-  if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value;
+class Decimal {
+  success: boolean;
+  value: BigInt;
+}
+
+export function getTokenDecimals(id: Address): Decimal {
+  const contract = ERC20.bind(id);
+
+  const decimals = contract.try_decimals();
+
+  if (!decimals.reverted) {
+    return { success: true, value: BigInt.fromI32(decimals.value as i32) };
   }
-  return BigInt.fromI32(decimalValue as i32);
+
+  return { success: false, value: BigInt.fromI32(18) };
 }
