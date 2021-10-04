8a3783caef983bd22d7fac0a5dfbf2a5aef188bc
Matthew Lilley <hello@matthewlilley.com>
2021-10-04 21:15:40 +0100

    feat: implement network agnostic prepare command

--- the-graph/bin/index.js
+++ the-graph/bin/index.js
@@ -1,45 +1,52 @@
-const { Command } = require("commander");
-const { exec } = require("child_process");
+const { Command } = require('commander')
+const { exec } = require('child_process')
 
-const program = new Command();
+const program = new Command()
 
 program
   .command('prepare')
   .arguments('<subgraph> <network>')
   .action((subgraph, network) => {
-    console.log("prepare command called", { subgraph, network });
-  });
+    console.log('prepare command called', { subgraph, network })
+    exec(
+      `node_modules/.bin/mustache config/${network}.js subgraphs/${subgraph}/template.yaml > subgraphs/${subgraph}/subgraph.yaml`
+    )
+  })
 
 program
-.command('deploy')
-.arguments('<subgraph> <network>')
-.action((subgraph, network) => {
-  console.log("deploy command called", { subgraph, network });
-});
+  .command('deploy')
+  .arguments('<subgraph>')
+  .action((subgraph) => {
+    // TODO: Impl
+    console.log('deploy command called', { subgraph })
+  })
 
 program
-.command('log')
-.arguments('<subgraphName>')
-.option('-v, --version <version>', 'Which version to get logs for', 'pending')
-.action((subgraphName = 'sushiswap/trident', options) => {
-  // console.log("log command called", { subgraphName, options });
-
-  const method = {
-    current: 'indexingStatusForCurrentVersion',
-    pending: 'indexingStatusForPendingVersion'
-  }
-
-  exec(`curl --location --request POST 'https://api.thegraph.com/index-node/graphql' --data '{"query":"{ ${method[options.version]}(subgraphName: \\"${subgraphName}\\") { subgraph synced fatalError { message } nonFatalErrors { message } } }"}'`, (error, data, getter) => {
-    if(error){
-      console.log("error",error.message);
-      return;
+  .command('log')
+  .arguments('<subgraphName>')
+  .option('-v, --version <version>', 'Which version to get logs for', 'pending')
+  .action((subgraphName = 'sushiswap/trident', options) => {
+    const method = {
+      current: 'indexingStatusForCurrentVersion',
+      pending: 'indexingStatusForPendingVersion',
     }
-    if(getter){
-      console.log("data",data);
-      return;
-    }
-    console.log("data",data);
-  });
-});
 
-program.parse(process.argv);
+    exec(
+      `curl --location --request POST 'https://api.thegraph.com/index-node/graphql' --data '{"query":"{ ${
+        method[options.version]
+      }(subgraphName: \\"${subgraphName}\\") { subgraph synced fatalError { message } nonFatalErrors { message } } }"}'`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log('error', error.message)
+          return
+        }
+        if (stderr) {
+          console.log('data', stdout)
+          return
+        }
+        console.log('data', stdout)
+      }
+    )
+  })
+
+program.parse(process.argv)
