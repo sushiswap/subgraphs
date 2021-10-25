cc43b562b2ff71ccd8a49a94075a8daa5a695a39
Matthew Lilley <hello@matthewlilley.com>
2021-10-25 17:12:10 +0100

    config: update kovan config

--- the-graph/config/kovan.js
+++ the-graph/config/kovan.js
@@ -3,8 +3,8 @@ module.exports = {
   sushi: { address: '0x0769fd68dfb93167989c6f7254cd0d766fb2841f' },
   weth: { address: '0xc778417e063141139fce010982780140aa0cd5ab' },
   masterDeployer: {
-    address: '0xB8ef8579EEBDCCCf69305418E49a3A545301b393',
-    startBlock: 27109220,
+    address: '0xBe8bdb5A8341808e4665207C77B76aDC30598EAE',
+    startBlock: 27660363,
   },
   array: [
     // ...
