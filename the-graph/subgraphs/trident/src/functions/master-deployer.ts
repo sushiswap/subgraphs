6b23edafb4868b0028429ede404b97036facc335
Matthew Lilley <hello@matthewlilley.com>
2021-10-25 17:16:39 +0100

    chore: update master deployer functions

--- the-graph/subgraphs/trident/src/functions/master-deployer.ts
+++ the-graph/subgraphs/trident/src/functions/master-deployer.ts
@@ -1,6 +1,5 @@
-import { ADDRESS_ZERO, MASTER_DEPLOYER_ADDRESS } from '../constants'
-import { Address, BigInt } from '@graphprotocol/graph-ts'
-
+import { Address } from '@graphprotocol/graph-ts'
+import { MASTER_DEPLOYER_ADDRESS } from '../constants'
 import { MasterDeployer } from '../../generated/schema'
 import { MasterDeployer as MasterDeployerContract } from '../../generated/MasterDeployer/MasterDeployer'
 
@@ -11,7 +10,6 @@ export function getOrCreateMasterDeployer(id: Address = MASTER_DEPLOYER_ADDRESS)
     const contract = MasterDeployerContract.bind(id)
     masterDeployer = new MasterDeployer(id.toHex())
     masterDeployer.owner = contract.owner()
-    masterDeployer.migrator = ADDRESS_ZERO
     masterDeployer.barFee = contract.barFee()
     masterDeployer.barFeeTo = contract.barFeeTo()
     masterDeployer.bento = contract.bento()
