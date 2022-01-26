ff6daffa11d603fb01e54eb16691945529f1fa07
OlaStenberg <hello@olastenberg.com>
2022-01-26 23:17:20 +0100

    cleanup unused functions and unused imports

--- the-graph/subgraphs/trident/tests/mocks.ts
+++ the-graph/subgraphs/trident/tests/mocks.ts
@@ -1,25 +1,7 @@
-import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts'
+import { Address, Bytes, ethereum } from '@graphprotocol/graph-ts'
 import { newMockEvent } from 'matchstick-as'
-import { AddToWhitelist, BarFeeUpdated, DeployPool } from '../generated/MasterDeployer/MasterDeployer'
+import { AddToWhitelist, DeployPool } from '../generated/MasterDeployer/MasterDeployer'
 
-
-export function createBarFeeUpdatedEvent(fee: number, ownerAddress: Address): BarFeeUpdated {
-  let mockEvent = newMockEvent()
-  let event = new BarFeeUpdated(
-    ownerAddress,
-    mockEvent.logIndex,
-    mockEvent.transactionLogIndex,
-    mockEvent.logType,
-    mockEvent.block,
-    mockEvent.transaction,
-    mockEvent.parameters
-  )
-  event.parameters = new Array()
-  let feeParam = new ethereum.EventParam('barFee', ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(0)))
-  event.parameters.push(feeParam)
-
-  return event
-}
 
 export function createAddToWhitelistEvent(factory: Address, ownerAddress: Address): AddToWhitelist {
   let mockEvent = newMockEvent()
@@ -62,24 +44,6 @@ export function createDeployPoolEvent(
   event.parameters.push(factoryParam)
   event.parameters.push(poolParam)
   event.parameters.push(deployDataParam)
-
-  return event
-}
-
-export function createMintEvent(): DeployPool {
-  let mockEvent = newMockEvent()
-  let event = new DeployPool(
-    mockEvent.address,
-    mockEvent.logIndex,
-    mockEvent.transactionLogIndex,
-    mockEvent.logType,
-    mockEvent.block,
-    mockEvent.transaction,
-    mockEvent.parameters
-  )
-  event.parameters = new Array()
-  let addressParam = new ethereum.EventParam('address', ethereum.Value.fromAddress(mockEvent.address))
-  event.parameters.push(addressParam)
 
   return event
 }
