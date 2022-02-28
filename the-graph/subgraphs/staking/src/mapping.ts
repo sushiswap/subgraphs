81de69d68f72ba1e31ea6d0164553bdccf80a846
OlaStenberg <hello@olastenberg.com>
2022-02-28 14:38:40 +0100

    feat: schema first draft

--- the-graph/subgraphs/staking/src/mapping.ts
+++ the-graph/subgraphs/staking/src/mapping.ts
@@ -12,28 +12,28 @@ import { BigInt } from '@graphprotocol/graph-ts'
 import { ExampleEntity } from '../generated/schema'
 
 export function onIncentiveCreated(event: IncentiveCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // // Entities can be loaded from the store using a string ID; this ID
+  // // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.token = event.params.token
-  entity.rewardToken = event.params.rewardToken
+  // // Entity fields can be set based on event parameters
+  // entity.token = event.params.token
+  // entity.rewardToken = event.params.rewardToken
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
