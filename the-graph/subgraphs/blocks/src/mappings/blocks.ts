e32edcc210c5e9f84f895609672dfb705e881fb3
OlaStenberg <hello@olastenberg.com>
2022-03-03 10:14:34 +0100

    refactoring: rename handler from handleBlock to onBlock

--- the-graph/subgraphs/blocks/src/mappings/blocks.ts
+++ the-graph/subgraphs/blocks/src/mappings/blocks.ts
@@ -1,10 +1,10 @@
 import { ethereum } from '@graphprotocol/graph-ts'
 import { Block } from '../../generated/schema'
 
-export function handleBlock(block: ethereum.Block): void {
+export function onBlock(block: ethereum.Block): void {
   let id = block.hash.toHex()
   let blockEntity = new Block(id)
-  
+
   blockEntity.number = block.number
   blockEntity.timestamp = block.timestamp
   blockEntity.parentHash = block.parentHash.toHex()
