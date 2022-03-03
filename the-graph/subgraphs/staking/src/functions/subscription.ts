863eddc1492c822ef4862064d75fc072fc4182d8
OlaStenberg <hello@olastenberg.com>
2022-03-03 22:42:55 +0100

    refactoring: rename Subscription to _Subscription (ReservedName in GraphQL)

--- the-graph/subgraphs/staking/src/functions/subscription.ts
+++ the-graph/subgraphs/staking/src/functions/subscription.ts
@@ -1,29 +1,22 @@
-import { Subscription } from '../../generated/schema'
+import { _Subscription } from '../../generated/schema'
 
-export function getOrCreateSubscription(userId: string, count: string): Subscription {
+export function getOrCreateSubscription(userId: string, count: string): _Subscription {
   let id = getSubscriptionId(userId, count)
-  let subscription = Subscription.load(id)
+  let subscription = _Subscription.load(id)
 
   if (subscription === null) {
-    subscription = new Subscription(id)
+    subscription = new _Subscription(id)
     subscription.save()
   }
 
-  return subscription as Subscription
+  return subscription as _Subscription
 }
 
-export function getSubscription(userId: string, count: string): Subscription | null {
+export function getSubscription(userId: string, count: string): _Subscription | null {
   let id = getSubscriptionId(userId, count)
-  return Subscription.load(id)
+  return _Subscription.load(id)
 }
 
 export function getSubscriptionId(userId: string, count: string): string {
   return userId.concat(':').concat(count)
-}
-
-export function isSubscribed(userId: string, count: string): boolean {
-  let id = getSubscriptionId(userId, count)
-  let subscription = Subscription.load(id)
-
-  return subscription !== null
 }
