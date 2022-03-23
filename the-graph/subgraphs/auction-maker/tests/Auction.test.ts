01fdfdf31a227c9b806c67949b07d0a6d79abd60
OlaStenberg <hello@olastenberg.com>
2022-03-23 22:02:29 +0100

    test: bid updates the auction

--- the-graph/subgraphs/auction-maker/tests/Auction.test.ts
+++ the-graph/subgraphs/auction-maker/tests/Auction.test.ts
@@ -1,28 +1,71 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
-import { assert, test } from 'matchstick-as/assembly/index'
-import { onStart } from '../src/mappings/auction-maker'
+import { assert, test, clearStore } from 'matchstick-as/assembly/index'
+import { onPlaceBid, onCreateAuction } from '../src/mappings/auction-maker'
 import { MAX_TTL, MIN_TTL } from '../src/mappings/constants'
-import { createStartEvent } from './mocks'
+import { createPlaceBidEvent, createAuctionEvent } from './mocks'
+
+const ALICE = Address.fromString('0x00000000000000000000000000000000000a71ce')
+const BOB = Address.fromString('0x0000000000000000000000000000000000000b0b')
+const TOKEN = Address.fromString('0x0000000000000000000000000000000000000001')
+const AMOUNT = BigInt.fromString('1337')
+const REWARD_AMOUNT = BigInt.fromString('420')
+function cleanup(): void {
+  clearStore()
+}
 
 test('Auction is created', () => {
-    const alice = Address.fromString('0x00000000000000000000000000000000000a71ce')
-    const token = Address.fromString('0x0000000000000000000000000000000000000001')
-    const amount = BigInt.fromString("1337")
-    const rewardAmount = BigInt.fromString("420")
-    let event = createStartEvent(token, alice, amount, rewardAmount)
+  let event = createAuctionEvent(TOKEN, ALICE, AMOUNT, REWARD_AMOUNT)
 
-    onStart(event)
+  onCreateAuction(event)
 
-    const id = token.toHex()
-    assert.fieldEquals('Auction', id, 'id', id)
-    assert.fieldEquals('Auction', id, 'highestBidder', alice.toHex())
-    assert.fieldEquals('Auction', id, 'bidAmount', amount.toString())
-    assert.fieldEquals('Auction', id, 'rewardAmount', rewardAmount.toString())
-    assert.fieldEquals('Auction', id, 'minTTL', event.block.timestamp.plus(MIN_TTL).toString())
-    assert.fieldEquals('Auction', id, 'maxTTL', event.block.timestamp.plus(MAX_TTL).toString())
-    assert.fieldEquals('Auction', id, 'createdAtBlock', event.block.number.toString())
-    assert.fieldEquals('Auction', id, 'createdAtTimestamp', event.block.timestamp.toString())
-    assert.fieldEquals('Auction', id, 'modifiedAtTimestamp', event.block.number.toString())
-    assert.fieldEquals('Auction', id, 'modifiedAtBlock', event.block.timestamp.toString())
+  const id = TOKEN.toHex()
+  assert.fieldEquals('Auction', id, 'id', id)
+  assert.fieldEquals('Auction', id, 'highestBidder', ALICE.toHex())
+  assert.fieldEquals('Auction', id, 'bidAmount', AMOUNT.toString())
+  assert.fieldEquals('Auction', id, 'rewardAmount', REWARD_AMOUNT.toString())
+  assert.fieldEquals('Auction', id, 'minTTL', event.block.timestamp.plus(MIN_TTL).toString())
+  assert.fieldEquals('Auction', id, 'maxTTL', event.block.timestamp.plus(MAX_TTL).toString())
+  assert.fieldEquals('Auction', id, 'createdAtBlock', event.block.number.toString())
+  assert.fieldEquals('Auction', id, 'createdAtTimestamp', event.block.timestamp.toString())
+  assert.fieldEquals('Auction', id, 'modifiedAtTimestamp', event.block.number.toString())
+  assert.fieldEquals('Auction', id, 'modifiedAtBlock', event.block.timestamp.toString())
+
+  cleanup()
 })
 
+test('Bid updates the auction', () => {
+  let auctionEvent = createAuctionEvent(TOKEN, ALICE, AMOUNT, REWARD_AMOUNT)
+  let bidEvent = createPlaceBidEvent(TOKEN, BOB, AMOUNT)
+  const id = TOKEN.toHex()
+
+  // When: an auction event occurs
+  onCreateAuction(auctionEvent)
+
+  // Then: the auction entity contains the expected field values
+  let maxTTL = auctionEvent.block.timestamp.plus(MAX_TTL).toString()
+  assert.fieldEquals('Auction', id, 'highestBidder', ALICE.toHex())
+  assert.fieldEquals('Auction', id, 'bidAmount', AMOUNT.toString())
+  assert.fieldEquals('Auction', id, 'minTTL', auctionEvent.block.timestamp.plus(MIN_TTL).toString())
+  assert.fieldEquals('Auction', id, 'maxTTL', maxTTL)
+  assert.fieldEquals('Auction', id, 'modifiedAtTimestamp', auctionEvent.block.number.toString())
+  assert.fieldEquals('Auction', id, 'modifiedAtBlock', auctionEvent.block.timestamp.toString())
+
+  // When: A bid occurs
+  bidEvent.block.timestamp = BigInt.fromString('1648063447')
+  bidEvent.block.number = BigInt.fromString('14444408')
+  onPlaceBid(bidEvent)
+
+  // Then: the auctions highest bidder and amount is updated
+  assert.fieldEquals('Auction', id, 'highestBidder', BOB.toHex())
+  assert.fieldEquals('Auction', id, 'bidAmount', AMOUNT.toString())
+
+  // And: the timestamps are also updated
+  assert.fieldEquals('Auction', id, 'minTTL', bidEvent.block.timestamp.plus(MIN_TTL).toString())
+  assert.fieldEquals('Auction', id, 'modifiedAtTimestamp', bidEvent.block.timestamp.toString())
+  assert.fieldEquals('Auction', id, 'modifiedAtBlock', bidEvent.block.number.toString())
+
+  // And: maxTTL remain unchanged
+  assert.fieldEquals('Auction', id, 'maxTTL', maxTTL)
+
+  cleanup()
+})
--- the-graph/subgraphs/auction-maker/tests/mocks.ts
+++ the-graph/subgraphs/auction-maker/tests/mocks.ts
@@ -1,10 +1,10 @@
 import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
 import { newMockEvent } from 'matchstick-as'
-import { Started as StartEvent } from '../generated/AuctionMaker/AuctionMaker'
+import { PlacedBid as PlaceBidEvent, Started as CreateAuctionEvent } from '../generated/AuctionMaker/AuctionMaker'
 
-export function createStartEvent(token: Address, bidder: Address, bidAmount: BigInt, rewardAmount: BigInt): StartEvent {
+export function createAuctionEvent(token: Address, bidder: Address, bidAmount: BigInt, rewardAmount: BigInt): CreateAuctionEvent {
   let mockEvent = newMockEvent()
-  let event = new StartEvent(
+  let event = new CreateAuctionEvent(
     mockEvent.address,
     mockEvent.logIndex,
     mockEvent.transactionLogIndex,
@@ -23,6 +23,30 @@ export function createStartEvent(token: Address, bidder: Address, bidAmount: Big
   event.parameters.push(bidderParam)
   event.parameters.push(bidAmountParam)
   event.parameters.push(rewardAmountParam)
+
+  return event
+}
+
+
+export function createPlaceBidEvent(token: Address, bidder: Address, bidAmount: BigInt): PlaceBidEvent {
+  let mockEvent = newMockEvent()
+  let event = new PlaceBidEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  )
+  event.parameters = new Array()
+
+  let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
+  let bidderParam = new ethereum.EventParam('sender', ethereum.Value.fromAddress(bidder))
+  let bidAmountParam = new ethereum.EventParam('bidAmount', ethereum.Value.fromUnsignedBigInt(bidAmount))
+  event.parameters.push(tokenParam)
+  event.parameters.push(bidderParam)
+  event.parameters.push(bidAmountParam)
 
   return event
 }
