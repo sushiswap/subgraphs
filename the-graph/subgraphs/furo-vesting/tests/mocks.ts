5c2548843d50d63ff52b66941c56bb1ec65a1950
OlaStenberg <hello@olastenberg.com>
2022-03-27 17:19:06 +0200

    test: add mocks for events

--- the-graph/subgraphs/furo-vesting/tests/mocks.ts
+++ the-graph/subgraphs/furo-vesting/tests/mocks.ts
@@ -1,139 +1,142 @@
-// import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
-// import { createMockedFunction, newMockEvent } from 'matchstick-as'
-// import {
-//   LogCancelStream as CancelStreamEvent,
-//   LogCreateStream as CreateStreamEvent,
-//   LogWithdrawFromStream as WithdrawEvent,
-// } from './FuroStream/FuroStream'
+import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
+import { createMockedFunction, newMockEvent } from 'matchstick-as'
+import {
+  LogCreateVesting as CreateVestingEvent,
+  LogStopVesting as CancelVestingEvent,
+  LogWithdraw as WithdrawEvent,
+} from '../generated/FuroVesting/FuroVesting'
 
-// export function createStreamEvent(
-//   streamId: BigInt,
-//   sender: Address,
-//   recipient: Address,
-//   token: Address,
-//   amount: BigInt,
-//   startTime: BigInt,
-//   endTime: BigInt,
-//   fromBentoBox: boolean
-// ): CreateStreamEvent {
-//   let mockEvent = newMockEvent()
+export function createVestingEvent(
+  vestId: BigInt,
+  token: Address,
+  sender: Address,
+  recipient: Address,
+  startTime: BigInt,
+  cliffDuration: BigInt,
+  stepDuration: BigInt,
+  steps: BigInt,
+  cliffAmount: BigInt,
+  stepAmount: BigInt,
+  fromBentoBox: boolean
+): CreateVestingEvent {
+  let mockEvent = newMockEvent()
 
-//   let event = new CreateStreamEvent(
-//     mockEvent.address,
-//     mockEvent.logIndex,
-//     mockEvent.transactionLogIndex,
-//     mockEvent.logType,
-//     mockEvent.block,
-//     mockEvent.transaction,
-//     mockEvent.parameters
-//   )
+  let event = new CreateVestingEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  )
 
-//   event.parameters = new Array()
-//   let streamIdParam = new ethereum.EventParam('streamId', ethereum.Value.fromUnsignedBigInt(streamId))
-//   let senderParam = new ethereum.EventParam('sender', ethereum.Value.fromAddress(sender))
-//   let recipientParam = new ethereum.EventParam('recipient', ethereum.Value.fromAddress(recipient))
-//   let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
-//   let amountParam = new ethereum.EventParam('amount', ethereum.Value.fromUnsignedBigInt(amount))
-//   let startTimeParam = new ethereum.EventParam('startTime', ethereum.Value.fromUnsignedBigInt(startTime))
-//   let endTimeParam = new ethereum.EventParam('endTime', ethereum.Value.fromUnsignedBigInt(endTime))
-//   let fromBentoBoxParam = new ethereum.EventParam('fromBentoBox', ethereum.Value.fromBoolean(fromBentoBox))
+  event.parameters = new Array()
+  let vestIdParam = new ethereum.EventParam('vestId', ethereum.Value.fromUnsignedBigInt(vestId))
+  let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
+  let senderParam = new ethereum.EventParam('sender', ethereum.Value.fromAddress(sender))
+  let recipientParam = new ethereum.EventParam('recipient', ethereum.Value.fromAddress(recipient))
+  let startTimeParam = new ethereum.EventParam('startTime', ethereum.Value.fromUnsignedBigInt(startTime))
+  let cliffDurationParam = new ethereum.EventParam('cliffDuration', ethereum.Value.fromUnsignedBigInt(cliffDuration))
+  let stepDurationParam = new ethereum.EventParam('stepDuration', ethereum.Value.fromUnsignedBigInt(stepDuration))
+  let stepsParam = new ethereum.EventParam('steps', ethereum.Value.fromUnsignedBigInt(steps))
+  let cliffAmountParam = new ethereum.EventParam('cliffAmount', ethereum.Value.fromUnsignedBigInt(cliffAmount))
+  let stepAmountParam = new ethereum.EventParam('stepAmount', ethereum.Value.fromUnsignedBigInt(stepAmount))
+  let fromBentoBoxParam = new ethereum.EventParam('fromBentoBox', ethereum.Value.fromBoolean(fromBentoBox))
 
-//   event.parameters.push(streamIdParam)
-//   event.parameters.push(senderParam)
-//   event.parameters.push(recipientParam)
-//   event.parameters.push(tokenParam)
-//   event.parameters.push(amountParam)
-//   event.parameters.push(startTimeParam)
-//   event.parameters.push(endTimeParam)
-//   event.parameters.push(fromBentoBoxParam)
+  event.parameters.push(vestIdParam)
+  event.parameters.push(tokenParam)
+  event.parameters.push(senderParam)
+  event.parameters.push(recipientParam)
+  event.parameters.push(startTimeParam)
+  event.parameters.push(cliffDurationParam)
+  event.parameters.push(stepDurationParam)
+  event.parameters.push(stepsParam)
+  event.parameters.push(cliffAmountParam)
+  event.parameters.push(stepAmountParam)
+  event.parameters.push(fromBentoBoxParam)
 
-//   return event
-// }
+  return event
+}
 
-// export function createWithdrawEvent(
-//   streamId: BigInt,
-//   sharesToWithdraw: BigInt,
-//   withdrawTo: Address,
-//   token: Address,
-//   toBentoBox: boolean
-// ): WithdrawEvent {
-//   let mockEvent = newMockEvent()
+export function createWithdrawEvent(
+  vestId: BigInt,
+  amount: BigInt,
+  token: Address,
+  toBentoBox: boolean
+): WithdrawEvent {
+  let mockEvent = newMockEvent()
 
-//   let event = new WithdrawEvent(
-//     mockEvent.address,
-//     mockEvent.logIndex,
-//     mockEvent.transactionLogIndex,
-//     mockEvent.logType,
-//     mockEvent.block,
-//     mockEvent.transaction,
-//     mockEvent.parameters
-//   )
+  let event = new WithdrawEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  )
 
-//   event.parameters = new Array()
-//   let streamIdParam = new ethereum.EventParam('streamId', ethereum.Value.fromUnsignedBigInt(streamId))
-//   let sharesToWithdrawParam = new ethereum.EventParam(
-//     'sharesToWithdraw',
-//     ethereum.Value.fromUnsignedBigInt(sharesToWithdraw)
-//   )
-//   let withdrawToParam = new ethereum.EventParam('withdrawTo', ethereum.Value.fromAddress(withdrawTo))
-//   let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
-//   let toBentoBoxParam = new ethereum.EventParam('toBentoBox', ethereum.Value.fromBoolean(toBentoBox))
+  event.parameters = new Array()
+  let vestIdParam = new ethereum.EventParam('vestId', ethereum.Value.fromUnsignedBigInt(vestId))
+  let amountParam = new ethereum.EventParam('amount', ethereum.Value.fromUnsignedBigInt(amount))
+  let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
+  let toBentoBoxParam = new ethereum.EventParam('toBentoBox', ethereum.Value.fromBoolean(toBentoBox))
 
-//   event.parameters.push(streamIdParam)
-//   event.parameters.push(sharesToWithdrawParam)
-//   event.parameters.push(withdrawToParam)
-//   event.parameters.push(tokenParam)
-//   event.parameters.push(toBentoBoxParam)
+  event.parameters.push(vestIdParam)
+  event.parameters.push(amountParam)
+  event.parameters.push(tokenParam)
+  event.parameters.push(toBentoBoxParam)
 
-//   return event
-// }
+  return event
+}
 
-// export function createCancelStreamEvent(
-//   streamId: BigInt,
-//   senderBalance: BigInt,
-//   recipientBalance: BigInt,
-//   token: Address,
-//   toBentoBox: boolean
-// ): CancelStreamEvent {
-//   let mockEvent = newMockEvent()
+export function createCancelStreamEvent(
+  vestId: BigInt,
+  ownerAmount: BigInt,
+  recipientAmount: BigInt,
+  token: Address,
+  toBentoBox: boolean
+): CancelVestingEvent {
+  let mockEvent = newMockEvent()
 
-//   let event = new CancelStreamEvent(
-//     mockEvent.address,
-//     mockEvent.logIndex,
-//     mockEvent.transactionLogIndex,
-//     mockEvent.logType,
-//     mockEvent.block,
-//     mockEvent.transaction,
-//     mockEvent.parameters
-//   )
+  let event = new CancelVestingEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  )
 
-//   event.parameters = new Array()
-//   let streamIdParam = new ethereum.EventParam('streamId', ethereum.Value.fromUnsignedBigInt(streamId))
-//   let senderBalanceParam = new ethereum.EventParam('senderBalance', ethereum.Value.fromUnsignedBigInt(senderBalance))
-//   let recipientBalanceParam = new ethereum.EventParam(
-//     'recipientBalance',
-//     ethereum.Value.fromUnsignedBigInt(recipientBalance)
-//   )
-//   let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
-//   let toBentoBoxParam = new ethereum.EventParam('toBentoBox', ethereum.Value.fromBoolean(toBentoBox))
+  event.parameters = new Array()
+  let vestIdParam = new ethereum.EventParam('vestId', ethereum.Value.fromUnsignedBigInt(vestId))
+  let ownerAmountParam = new ethereum.EventParam('ownerAmount', ethereum.Value.fromUnsignedBigInt(ownerAmount))
+  let recipientAmountParam = new ethereum.EventParam(
+    'recipientAmount',
+    ethereum.Value.fromUnsignedBigInt(recipientAmount)
+  )
+  let tokenParam = new ethereum.EventParam('token', ethereum.Value.fromAddress(token))
+  let toBentoBoxParam = new ethereum.EventParam('toBentoBox', ethereum.Value.fromBoolean(toBentoBox))
 
-//   event.parameters.push(streamIdParam)
-//   event.parameters.push(senderBalanceParam)
-//   event.parameters.push(recipientBalanceParam)
-//   event.parameters.push(tokenParam)
-//   event.parameters.push(toBentoBoxParam)
+  event.parameters.push(vestIdParam)
+  event.parameters.push(ownerAmountParam)
+  event.parameters.push(recipientAmountParam)
+  event.parameters.push(tokenParam)
+  event.parameters.push(toBentoBoxParam)
 
-//   return event
-// }
+  return event
+}
 
-// export function createTokenMock(contractAddress: string, decimals: BigInt, name: string, symbol: string): void {
-//   createMockedFunction(Address.fromString(contractAddress), 'decimals', 'decimals():(uint8)').returns([
-//     ethereum.Value.fromUnsignedBigInt(decimals),
-//   ])
-//   createMockedFunction(Address.fromString(contractAddress), 'name', 'name():(string)').returns([
-//     ethereum.Value.fromString(name),
-//   ])
-//   createMockedFunction(Address.fromString(contractAddress), 'symbol', 'symbol():(string)').returns([
-//     ethereum.Value.fromString(symbol),
-//   ])
-// }
+export function createTokenMock(contractAddress: string, decimals: BigInt, name: string, symbol: string): void {
+  createMockedFunction(Address.fromString(contractAddress), 'decimals', 'decimals():(uint8)').returns([
+    ethereum.Value.fromUnsignedBigInt(decimals),
+  ])
+  createMockedFunction(Address.fromString(contractAddress), 'name', 'name():(string)').returns([
+    ethereum.Value.fromString(name),
+  ])
+  createMockedFunction(Address.fromString(contractAddress), 'symbol', 'symbol():(string)').returns([
+    ethereum.Value.fromString(symbol),
+  ])
+}
