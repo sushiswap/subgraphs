aa458b68a9c06b4f6fe99b8e3b7437f84943f6be
OlaStenberg <hello@olastenberg.com>
2022-03-28 16:02:48 +0200

    refactor: simplify schedule creation

--- the-graph/subgraphs/furo-vesting/src/functions/schedule.ts
+++ the-graph/subgraphs/furo-vesting/src/functions/schedule.ts
@@ -1,9 +1,36 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts'
-import { log } from 'matchstick-as'
-import { LogCreateVesting as CreateVestingEvent } from '../../generated/FuroVesting/FuroVesting'
+import { BigInt } from '@graphprotocol/graph-ts'
 import { Schedule, SchedulePeriod, Vesting } from '../../generated/schema'
 import { CLIFF, END, START, STEP } from '../constants'
-import { increaseUserCount } from './furo'
+
+export function createSchedule(vesting: Vesting): void {
+  let schedule = getOrCreateSchedule('1') // FIXME: waiting for event to change, hardcoded for now
+  schedule.vesting = '1'
+  schedule.save()
+
+  createSchedulePeriods(vesting)
+}
+
+function createSchedulePeriods(vesting: Vesting): void {
+  let passedTime = vesting.startedAt
+  let passedAmount = BigInt.fromU32(0)
+  let vestId = '1' // FIXME: hardcoded for now, change when event is updated
+  savePeriod(vestId, 0, START, passedTime, passedAmount)
+
+  passedTime = passedTime.plus(vesting.cliffDuration)
+  passedAmount = passedAmount.plus(vesting.cliffAmount)
+  savePeriod(vestId, 1, CLIFF, passedTime, passedAmount)
+
+  for (let i = 2; i < vesting.steps.toI32() - 1; i++) {
+    passedTime = passedTime.plus(vesting.stepDuration)
+    passedAmount = passedAmount.plus(vesting.stepAmount)
+    savePeriod(vestId, i, STEP, passedTime, passedAmount)
+  }
+
+  passedTime = passedTime.plus(vesting.stepDuration)
+  passedAmount = passedAmount.plus(vesting.stepAmount)
+
+  savePeriod(vestId, vesting.steps.toI32(), END, passedTime, passedAmount)
+}
 
 function getOrCreateSchedule(id: string): Schedule {
   let schedule = Schedule.load(id)
@@ -30,59 +57,10 @@ function getOrCreateSchedulePeriod(id: string, number: i32): SchedulePeriod {
   return period as SchedulePeriod
 }
 
-export function createSchedule(vesting: Vesting): void {
-  // let schedule = getOrCreateSchedule(event.params.vestId)
-  let schedule = getOrCreateSchedule('1') // FIXME: waiting for event to change, hardcoded for now
-  schedule.vesting = '1'
-  schedule.save()
-
-  createSchedulePeriods(vesting)
-}
-
-function createSchedulePeriods(vesting: Vesting): void {
-  let passedTime = vesting.startedAt
-  let passedAmount = BigInt.fromU32(0)
-
-  let startPeriod = getOrCreateSchedulePeriod('1', 0)
-  startPeriod.type = START
-  startPeriod.time = passedTime
-  startPeriod.amount = passedAmount
-  startPeriod.save()
-
-  // log.debug('TYPE TIME     AMOUNT', [])
-  // log.debug('{} {} {}', [START, passedTime.toString(), passedAmount.toString()])
-
-  passedTime = passedTime.plus(vesting.cliffDuration)
-  passedAmount = passedAmount.plus(vesting.cliffAmount)
-
-  let cliffPeriod = getOrCreateSchedulePeriod('1', 1)
-  cliffPeriod.type = CLIFF
-  cliffPeriod.time = passedTime
-  cliffPeriod.amount = passedAmount
-  cliffPeriod.save()
-
-  // log.debug('{} {} {}', [CLIFF, passedTime.toString(), passedAmount.toString()])
-  for (let i = 2; i < vesting.steps.toI32() - 1; i++) {
-    passedTime = passedTime.plus(vesting.stepDuration)
-    passedAmount = passedAmount.plus(vesting.stepAmount)
-
-    let stepPeriod = getOrCreateSchedulePeriod('1', i)
-    stepPeriod.type = STEP
-    stepPeriod.time = passedTime
-    stepPeriod.amount = passedAmount
-    stepPeriod.save()
-
-    // log.debug('{}  {} {}', [STEP, passedTime.toString(), passedAmount.toString()])
-  }
-
-  passedTime = passedTime.plus(vesting.stepDuration)
-  passedAmount = passedAmount.plus(vesting.stepAmount)
-
-  let endPeriod = getOrCreateSchedulePeriod('1', vesting.steps.toI32())
-  endPeriod.type = END
-  endPeriod.time = passedTime
-  endPeriod.amount = passedAmount
-  endPeriod.save()
-
-  // log.debug('{}   {} {}', [END, passedTime.toString(), passedAmount.toString()])
+function savePeriod(vestId: string, number: i32, type: string, time: BigInt, amount: BigInt): void {
+  let period = getOrCreateSchedulePeriod(vestId, number)
+  period.type = type
+  period.time = time
+  period.amount = amount
+  period.save()
 }
