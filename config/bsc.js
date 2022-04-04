72014b35aea25512fcd289c778d3d596158cb921
Matthew Lilley <hello@matthewlilley.com>
2022-04-04 02:45:35 +0100

    chore: add bentobox configs for other networks

--- config/bsc.js
+++ config/bsc.js
@@ -1,5 +1,9 @@
 module.exports = {
   network: 'bsc',
+  bentobox: {
+    address: '0xf5bce5077908a1b7370b9ae04adc565ebd643966',
+    startBlock: 5926250,
+  },
   legacy: {
     factory: {
       address: '0xc35dadb65012ec5796536bd9864ed8773abc74c4',
--- config/fantom.js
+++ config/fantom.js
@@ -1,5 +1,9 @@
 module.exports = {
   network: 'fantom',
+  bentobox: {
+    address: '0xf5bce5077908a1b7370b9ae04adc565ebd643966',
+    startBlock: 2918231,
+  },
   legacy: {
     factory: {
       address: '0xc35dadb65012ec5796536bd9864ed8773abc74c4',
