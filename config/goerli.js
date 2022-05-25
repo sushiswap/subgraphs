c222bdfd12f7da356c4a31d5195e8b3eee6ebb65
OlaStenberg <hello@olastenberg.com>
2022-05-25 19:13:38 +0200

    chore(Furo): update furo vesting addresses

--- config/goerli.js
+++ config/goerli.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   furo: {
     stream: { address: '0x3C4bc596C4946d812D0eA77940038576f228faC7', startBlock: 6929553 },
-    vesting: { address: '0xb449F33126a2b17526B48a962c15da0dd2D1fb44', startBlock: 6929559 },
+    vesting: { address: '0x344b7c97f1142d6743d9e5C38E81eCe46eAb68BA', startBlock: 6947498 },
   },
   auctionMaker: { address: '0x0000000000000000000000000000000000000000', startBlock: 0 },
   staking: { address: '0x0000000000000000000000000000000000000000', startBlock: 0 },
--- config/kovan.js
+++ config/kovan.js
@@ -50,7 +50,7 @@ module.exports = {
   },
   furo: {
     stream: { address: '0xa60f90530E9412Fe1a19ff91F1491f1738953B7C', startBlock: 31685113 },
-    vesting: { address: '0x09464506e15c72D9B3dc3272Fc00757982E99E9c', startBlock: 31695059 },
+    vesting: { address: '0x359E322451EaF32e793C557Fc2A31B5fb45FF2dd', startBlock: 31809076 },
   },
   auctionMaker: { address: '0x0000000000000000000000000000000000000000', startBlock: 0 },
   staking: { address: '0x0000000000000000000000000000000000000000', startBlock: 0 },
