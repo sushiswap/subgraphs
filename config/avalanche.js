6cd90e1c2abadf84a918cd1e874b955f8376d8c2
OlaStenberg <hello@olastenberg.com>
2022-06-15 14:55:01 +0200

    chore(subgraphs/exchange): add exchange config for avalanche and moonriver

--- config/avalanche.js
+++ config/avalanche.js
@@ -1,5 +1,35 @@
 module.exports = {
   network: 'avalanche',
+  native: { address: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7' },
+  whitelistedTokenAddresses: [
+    // WETH
+    '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab',
+    // USDC
+    '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664',
+    // USDT
+    '0xc7198437980c041c805a1edcba50c1ce5db95118',
+    // DAI
+    '0xd586e7f844cea2f87f50152665bcbc2c279d8d70',
+    // WBTC
+    '0x50b7545627a5162f82a992c33b87adc75187b218',
+    // MIM
+    '0x130966628846bfd36ff31a822705796e8cb8c18d',
+    // SUSHI
+    '0x37b608519f91f70f2eeb0e5ed9af4061722e4f76',
+    // TIME
+    '0xb54f16fb19478766a268f172c9480f8da1a7c9c3',
+    // SPELL
+    '0xce1bffbd5374dac86a2893119683f4911a2f7814',
+  ],
+  stableTokenAddresses: [
+    // USDC
+    '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664',
+    // USDT
+    '0xc7198437980c041c805a1edcba50c1ce5db95118',
+    // DAI
+    '0xd586e7f844cea2f87f50152665bcbc2c279d8d70',
+  ],
+  minimumNativeLiquidity: 3,
   bentobox: {
     address: '0x0711b6026068f736bae6b213031fce978d48e026',
     startBlock: 3672722,
@@ -12,6 +42,7 @@ module.exports = {
     minimum_usd_threshold_new_pairs: "3000",
     factory: {
       address: '0xc35dadb65012ec5796536bd9864ed8773abc74c4',
+      initCodeHash: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
       startBlock: 506190,
     },
   },
--- config/moonriver.js
+++ config/moonriver.js
@@ -1,5 +1,33 @@
 module.exports = {
   network: 'moonriver',
+  native: { address: '0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c' },
+  whitelistedTokenAddresses: [
+    // WETH
+    '0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c',
+    // USDC
+    '0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d',
+    // USDT
+    '0xb44a9b6905af7c801311e8f4e76932ee959c663c',
+    // DAI
+    '0x80a16016cc4a2e6a2caca8a4a498b1699ff0f844',
+    // WMOVR
+    '0xf50225a84382c74cbdea10b0c176f71fc3de0c4d',
+    // WBTC
+    '0xe6a991ffa8cfe62b0bf6bf72959a3d4f11b2e0f5',
+    // FRAX
+    '0x1a93b23281cc1cde4c4741353f3064709a16197d',
+    // MIM
+    '0x0cae51e1032e8461f4806e26332c030e34de3adb',
+  ],
+  stableTokenAddresses: [
+    // USDC
+    '0xe3f5a90f9cb311505cd691a46596599aa1a0ad7d',
+    // USDT
+    '0xb44a9b6905af7c801311e8f4e76932ee959c663c',
+    // DAI
+    '0x80a16016cc4a2e6a2caca8a4a498b1699ff0f844',
+  ],
+  minimumNativeLiquidity: 5,
   bentobox: {
     address: '0x145d82bca93cca2ae057d1c6f26245d1b9522e6f',
     startBlock: 1610292,
@@ -7,6 +35,14 @@ module.exports = {
   miso: {
     accessControls: { address: '0x0769fd68dfb93167989c6f7254cd0d766fb2841f', startBlock: 1610232 },
     market: { address: '0x120140d0c1ebc938befc84840575ecdc5fe55afe', startBlock: 1610328 },
+  },
+  legacy: {
+    minimum_usd_threshold_new_pairs: "3000",
+    factory: {
+      address: '0xc35dadb65012ec5796536bd9864ed8773abc74c4',
+      initCodeHash: '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+      startBlock: 428426,
+    },
   },
   blocks: {
     address: '0xc35dadb65012ec5796536bd9864ed8773abc74c4',
