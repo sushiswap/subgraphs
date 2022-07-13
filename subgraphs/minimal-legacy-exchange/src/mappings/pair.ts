570e771929a630027abd77de141362f04bee6a63
OlaStenberg <hello@olastenberg.com>
2022-07-13 11:50:03 +0200

    fix(subgraphs/mle): map pairkpi.liquidity on transfer event

--- subgraphs/minimal-legacy-exchange/src/mappings/pair.ts
+++ subgraphs/minimal-legacy-exchange/src/mappings/pair.ts
@@ -1,6 +1,8 @@
-import { BigDecimal } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { Transfer } from '../../generated/Factory/ERC20'
+import { PairKpi } from '../../generated/schema'
 import { Sync } from '../../generated/Factory/Pair'
-import { BIG_DECIMAL_ZERO } from '../constants'
+import { ADDRESS_ZERO, BIG_DECIMAL_ZERO } from '../constants'
 import { convertTokenToDecimal, getOrCreateBundle, getOrCreateToken, getPair } from '../functions'
 import { getPairKpi } from '../functions/pair-kpi'
 import { getNativePriceInUSD, updateTokenKpiPrice as updateTokenPrice } from '../pricing'
@@ -13,14 +15,16 @@ export function onSync(event: Sync): void {
   const token0 = getOrCreateToken(pair.token0)
   const token1 = getOrCreateToken(pair.token1)
 
-
-  const token0LiquidityDifference = convertTokenToDecimal(event.params.reserve0, token0.decimals).minus(pairKpi.token0Liquidity)
-  const token1LiquidityDifference = convertTokenToDecimal(event.params.reserve1, token1.decimals).minus(pairKpi.token1Liquidity)
+  const token0LiquidityDifference = convertTokenToDecimal(event.params.reserve0, token0.decimals).minus(
+    pairKpi.token0Liquidity
+  )
+  const token1LiquidityDifference = convertTokenToDecimal(event.params.reserve1, token1.decimals).minus(
+    pairKpi.token1Liquidity
+  )
 
   pairKpi.token0Liquidity = convertTokenToDecimal(event.params.reserve0, token0.decimals)
   pairKpi.token1Liquidity = convertTokenToDecimal(event.params.reserve1, token1.decimals)
 
-  
   if (pairKpi.token1Liquidity.notEqual(BIG_DECIMAL_ZERO)) {
     pairKpi.token0Price = pairKpi.token0Liquidity.div(pairKpi.token1Liquidity)
   } else {
@@ -51,4 +55,32 @@ export function onSync(event: Sync): void {
 
   pairKpi.liquidityUSD = pairKpi.liquidityNative.times(bundle.nativePrice)
   pairKpi.save()
+}
+
+export function onTransfer(event: Transfer): void {
+  if (isInitialTransfer(event)) {
+    return
+  }
+
+  const value = event.params.value.divDecimal(BigDecimal.fromString('1e18'))
+  const pairKpi = getPairKpi(event.address.toHex())
+  if (isMint(event)) {
+    pairKpi.liquidity = pairKpi.liquidity.plus(value)
+    pairKpi.save()
+  } else if (isBurn(event, pairKpi)) {
+    pairKpi.liquidity = pairKpi.liquidity.minus(value)
+    pairKpi.save()
+  }
+}
+
+function isInitialTransfer(event: Transfer): boolean {
+  return event.params.to == ADDRESS_ZERO && event.params.value.equals(BigInt.fromI32(1000))
+}
+
+function isMint(event: Transfer): boolean {
+  return event.params.from == ADDRESS_ZERO
+}
+
+function isBurn(event: Transfer, pairKpi: PairKpi): boolean {
+  return event.params.to == ADDRESS_ZERO && event.params.from.toHex() == pairKpi.id
 }
