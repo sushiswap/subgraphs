0c6b5d6b18c0059f1b466807e1d657fd2554efe3
OlaStenberg <hello@olastenberg.com>
2022-07-18 16:30:04 +0200

    test(subgraphs/furo): add vesting and transaction test

--- subgraphs/furo/tests/Transaction.test.ts
+++ subgraphs/furo/tests/Transaction.test.ts
@@ -1,14 +1,20 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { assert, clearStore, test } from 'matchstick-as'
 import { CreateStream as CreateStreamEvent } from '../generated/FuroStream/FuroStream'
-import { DEPOSIT, DISBURSEMENT, EXTEND, WITHDRAWAL } from './../src/constants'
+import { CreateVesting as CreateVestingEvent } from '../generated/FuroVesting/FuroVesting'
+import { WEEK, YEAR } from '../src/constants'
 import { onCancelStream, onCreateStream, onUpdateStream, onWithdrawStream } from '../src/mappings/stream'
+import { onCancelVesting, onCreateVesting, onWithdrawVesting } from '../src/mappings/vesting'
+import { DEPOSIT, DISBURSEMENT, EXTEND, WITHDRAWAL } from './../src/constants'
 import {
   createCancelStreamEvent,
+  createCancelVestingEvent,
   createStreamEvent,
   createTokenMock,
   createUpdateStreamEvent,
+  createVestingEvent,
   createWithdrawStreamEvent,
+  createWithdrawVestingEvent,
 } from './mocks'
 
 const WETH_ADDRESS = Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
@@ -18,9 +24,42 @@ const STREAM_ID = BigInt.fromString('1001')
 const AMOUNT = BigInt.fromString('1000000')
 const START_TIME = BigInt.fromString('1648297495') // 	Sat Mar 26 2022 12:24:55 GMT+0000
 const END_TIME = BigInt.fromString('1650972295') // 	Tue Apr 26 2022 11:24:55 GMT+0000, One month later
+
+const RECIPIENT = Address.fromString('0x0000000000000000000000000000000000000b0b')
+const VESTING_ID = BigInt.fromString('1')
+const CLIFF_AMOUNT = BigInt.fromString('100000000')
+const STEPS_AMOUNT = BigInt.fromString('10000000')
+const CLIFF_DURATION = BigInt.fromU32(YEAR)
+const biweekly = 2 * WEEK
+const STEP_DURATION = BigInt.fromU32(biweekly)
+const STEPS = BigInt.fromU32(26)
+
+const TOTAL_AMOUNT = CLIFF_AMOUNT.plus(STEPS.times(STEPS_AMOUNT)) // 100000000 + (26 * 10000000) = 360000000
+
+let vestingEvent: CreateVestingEvent
 let streamEvent: CreateStreamEvent
 
-function setup(): void {
+function setupVesting(): void {
+  vestingEvent = createVestingEvent(
+    VESTING_ID,
+    WETH_ADDRESS,
+    SENDER,
+    RECIPIENT,
+    START_TIME,
+    CLIFF_DURATION,
+    STEP_DURATION,
+    STEPS,
+    CLIFF_AMOUNT,
+    STEPS_AMOUNT,
+    true
+  )
+  createTokenMock(WETH_ADDRESS.toHex(), BigInt.fromString('18'), 'Wrapped Ether', 'WETH')
+  onCreateVesting(vestingEvent)
+}
+
+function setupStream(): void {
+  cleanup()
+
   streamEvent = createStreamEvent(STREAM_ID, SENDER, RECIEVER, WETH_ADDRESS, AMOUNT, START_TIME, END_TIME, true)
   createTokenMock(WETH_ADDRESS.toHex(), BigInt.fromString('18'), 'Wrapped Ether', 'WETH')
   onCreateStream(streamEvent)
@@ -31,7 +70,7 @@ function cleanup(): void {
 }
 
 test('on create stream event, a transaction entity is created', () => {
-  setup()
+  setupStream()
   const id = STREAM_ID.toString().concat(':tx:0')
 
   assert.fieldEquals('Transaction', id, 'id', id)
@@ -49,7 +88,7 @@ test('on create stream event, a transaction entity is created', () => {
 })
 
 test('Cancel stream', () => {
-  setup()
+  setupStream()
   const id1 = STREAM_ID.toString().concat(':tx:1')
   const id2 = STREAM_ID.toString().concat(':tx:2')
   const amount2 = BigInt.fromString('2000000')
@@ -84,7 +123,7 @@ test('Cancel stream', () => {
 })
 
 test('Withdraw from stream creates a Transaction', () => {
-  setup()
+  setupStream()
   const id = STREAM_ID.toString().concat(':tx:1')
   const amount2 = BigInt.fromString('2000')
   let withdrawalEvent = createWithdrawStreamEvent(STREAM_ID, amount2, RECIEVER, WETH_ADDRESS, true)
@@ -108,7 +147,7 @@ test('Withdraw from stream creates a Transaction', () => {
 })
 
 test('Update stream creates a transaction', () => {
-  setup()
+  setupStream()
   const id = STREAM_ID.toString().concat(':tx:1')
   const extendTime = BigInt.fromString('2628000') // a month in seconds
   let updateStreamEvent = createUpdateStreamEvent(STREAM_ID, AMOUNT, extendTime, true)
@@ -126,6 +165,81 @@ test('Update stream creates a transaction', () => {
   assert.fieldEquals('Transaction', id, 'txHash', updateStreamEvent.transaction.hash.toHex())
   assert.fieldEquals('Transaction', id, 'createdAtBlock', updateStreamEvent.block.number.toString())
   assert.fieldEquals('Transaction', id, 'createdAtTimestamp', updateStreamEvent.block.timestamp.toString())
+
+  cleanup()
+})
+
+test('Deposit transaction is created on vesting creation event', () => {
+  setupVesting()
+
+  const id = VESTING_ID.toString().concat(':tx:0')
+  assert.fieldEquals('Transaction', id, 'id', id)
+  assert.fieldEquals('Transaction', id, 'type', DEPOSIT)
+  assert.fieldEquals('Transaction', id, 'furo', VESTING_ID.toString())
+  assert.fieldEquals('Transaction', id, 'amount', TOTAL_AMOUNT.toString())
+  assert.fieldEquals('Transaction', id, 'to', RECIPIENT.toHex())
+  assert.fieldEquals('Transaction', id, 'token', WETH_ADDRESS.toHex())
+  assert.fieldEquals('Transaction', id, 'toBentoBox', 'true')
+  assert.fieldEquals('Transaction', id, 'txHash', vestingEvent.transaction.hash.toHex())
+  assert.fieldEquals('Transaction', id, 'createdAtBlock', vestingEvent.block.number.toString())
+  assert.fieldEquals('Transaction', id, 'createdAtTimestamp', vestingEvent.block.timestamp.toString())
+
+  cleanup()
+})
+
+test('Disbursement transactions are created when vesting is cancelled', () => {
+  setupVesting()
+  let recipientAmount = CLIFF_AMOUNT
+  let ownerAmount = TOTAL_AMOUNT.minus(recipientAmount)
+  let cancelVestingEvent = createCancelVestingEvent(VESTING_ID, ownerAmount, recipientAmount, WETH_ADDRESS, true)
+
+  onCancelVesting(cancelVestingEvent)
+
+  const id = VESTING_ID.toString().concat(':tx:1')
+  assert.fieldEquals('Transaction', id, 'id', id)
+  assert.fieldEquals('Transaction', id, 'type', DISBURSEMENT)
+  assert.fieldEquals('Transaction', id, 'furo', VESTING_ID.toString())
+  assert.fieldEquals('Transaction', id, 'amount', recipientAmount.toString())
+  assert.fieldEquals('Transaction', id, 'to', RECIPIENT.toHex())
+  assert.fieldEquals('Transaction', id, 'token', WETH_ADDRESS.toHex())
+  assert.fieldEquals('Transaction', id, 'toBentoBox', 'true')
+  assert.fieldEquals('Transaction', id, 'txHash', vestingEvent.transaction.hash.toHex())
+  assert.fieldEquals('Transaction', id, 'createdAtBlock', vestingEvent.block.number.toString())
+  assert.fieldEquals('Transaction', id, 'createdAtTimestamp', vestingEvent.block.timestamp.toString())
+
+  const id2 = VESTING_ID.toString().concat(':tx:2')
+  assert.fieldEquals('Transaction', id2, 'id', id2)
+  assert.fieldEquals('Transaction', id2, 'type', DISBURSEMENT)
+  assert.fieldEquals('Transaction', id2, 'furo', VESTING_ID.toString())
+  assert.fieldEquals('Transaction', id2, 'amount', ownerAmount.toString())
+  assert.fieldEquals('Transaction', id2, 'to', SENDER.toHex())
+  assert.fieldEquals('Transaction', id2, 'token', WETH_ADDRESS.toHex())
+  assert.fieldEquals('Transaction', id2, 'toBentoBox', 'true')
+  assert.fieldEquals('Transaction', id, 'txHash', vestingEvent.transaction.hash.toHex())
+  assert.fieldEquals('Transaction', id2, 'createdAtBlock', vestingEvent.block.number.toString())
+  assert.fieldEquals('Transaction', id2, 'createdAtTimestamp', vestingEvent.block.timestamp.toString())
+
+  cleanup()
+})
+
+test('Withdrawal event creates withdrawal transaction', () => {
+  setupVesting()
+  const amount = BigInt.fromString('1337420')
+  let withdrawalEvent = createWithdrawVestingEvent(VESTING_ID, WETH_ADDRESS, amount, true)
+
+  onWithdrawVesting(withdrawalEvent)
+
+  const id = VESTING_ID.toString().concat(':tx:1')
+  assert.fieldEquals('Transaction', id, 'id', id)
+  assert.fieldEquals('Transaction', id, 'type', WITHDRAWAL)
+  assert.fieldEquals('Transaction', id, 'furo', VESTING_ID.toString())
+  assert.fieldEquals('Transaction', id, 'amount', amount.toString())
+  assert.fieldEquals('Transaction', id, 'to', RECIPIENT.toHex())
+  assert.fieldEquals('Transaction', id, 'token', WETH_ADDRESS.toHex())
+  assert.fieldEquals('Transaction', id, 'toBentoBox', 'true')
+  assert.fieldEquals('Transaction', id, 'txHash', vestingEvent.transaction.hash.toHex())
+  assert.fieldEquals('Transaction', id, 'createdAtBlock', vestingEvent.block.number.toString())
+  assert.fieldEquals('Transaction', id, 'createdAtTimestamp', vestingEvent.block.timestamp.toString())
 
   cleanup()
 })
