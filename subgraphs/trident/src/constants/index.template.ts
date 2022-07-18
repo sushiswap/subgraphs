872de7d033a81b08bd1f243efe6888f2b537135c
matthewlilley <hello@matthewlilley.com>
2022-07-18 10:22:05 +0100

    chore: trident constnat template

--- subgraphs/trident/src/constants/index.template.ts
+++ subgraphs/trident/src/constants/index.template.ts
@@ -34,16 +34,18 @@ export function getCreate2Address(from: Bytes, salt: String, initCodeHash: Bytes
 
 export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
 
-export const MASTER_DEPLOYER_ADDRESS = Address.fromString('{{ masterDeployer.address }}')
+export const MASTER_DEPLOYER_ADDRESS = Address.fromString('{{ trident.masterDeployer.address }}')
 
-export const CONSTANT_PRODUCT_POOL_FACTORY_ADDRESS = Address.fromString('{{ constantProductPoolFactory.address }}')
+export const CONSTANT_PRODUCT_POOL_FACTORY_ADDRESS = Address.fromString(
+  '{{ trident.constantProductPoolFactory.address }}'
+)
 
-export const HYBRID_POOL_FACTORY_ADDRESS = Address.fromString('{{ hybridPoolFactory.address }}')
+export const HYBRID_POOL_FACTORY_ADDRESS = Address.fromString('{{ trident.hybridPoolFactory.address }}')
 
-export const INDEX_POOL_FACTORY_ADDRESS = Address.fromString('{{ indexPoolFactory.address }}')
+export const INDEX_POOL_FACTORY_ADDRESS = Address.fromString('{{ trident.indexPoolFactory.address }}')
 
 export const CONCENTRATED_LIQUIDITY_POOL_FACTORY_ADDRESS = Address.fromString(
-  '{{ concentratedLiquidityPoolFactory.address }}'
+  '{{ concentratedLiquidityPoolFactory.address }}{{^concentratedLiquidityPoolFactory.address}}0x0000000000000000000000000000000000000000{{/concentratedLiquidityPoolFactory.address}}'
 )
 
 export const NATIVE_ADDRESS = '{{ trident.native.address }}'
@@ -52,18 +54,16 @@ export const WHITELISTED_TOKEN_ADDRESSES: string[] = '{{ trident.whitelistedToke
 
 export const STABLE_TOKEN_ADDRESSES: string[] = '{{ trident.stableTokenAddresses }}'.split(',')
 
-// export const STABLE_POOL_ADDRESSES: string[] = '{{ trident.stablePoolAddresses }}'.split(',')
-
 const STABLE_POOL_PERMUTATIONS = combinate(STABLE_TOKEN_ADDRESSES, NATIVE_ADDRESS, [false, true], [1, 5, 10, 30, 100])
 
 export const STABLE_POOL_ADDRESSES: string[] = STABLE_POOL_PERMUTATIONS.map<string>((perm: CombinateReturn) => {
-  const factory = Bytes.fromByteArray(Bytes.fromHexString('{{ constantProductPoolFactory.address }}'))
+  const factory = Bytes.fromByteArray(Bytes.fromHexString('{{ trident.constantProductPoolFactory.address }}'))
   const token0 = perm.tokens[0].slice(2).padStart(64, '0')
   const token1 = perm.tokens[1].slice(2).padStart(64, '0')
   const fee = (perm.fee as i32).toString(16).padStart(64, '0')
   const oracle = (+perm.oracle as i32).toString(16).padStart(64, '0')
   const keccak = crypto.keccak256(ByteArray.fromHexString('0x' + token0 + token1 + fee + oracle)).toHex()
-  const initCodeHash = Bytes.fromByteArray(Bytes.fromHexString('{{ constantProductPoolFactory.initCodeHash }}'))
+  const initCodeHash = Bytes.fromByteArray(Bytes.fromHexString('{{ trident.constantProductPoolFactory.initCodeHash }}'))
 
   return getCreate2Address(factory, keccak, initCodeHash).toHex()
 })
