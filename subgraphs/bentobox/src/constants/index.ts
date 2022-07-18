b85d8b4ab982d7a2cb1fe040807adbeadd31432b
matthewlilley <hello@matthewlilley.com>
2022-07-18 10:21:48 +0100

    chore: rm tracked cached codegen files

--- subgraphs/bentobox/src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from '@graphprotocol/graph-ts'
-
-export const BENTOBOX_ADDRESS = Address.fromString('0x0319000133d3ada02600f0875d2cf03d442c3367')
-
-export * from './transaction-types'
