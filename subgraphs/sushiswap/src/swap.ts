5effdda5fd1a0ec481c0fe36c660ebaae45bc8f5
Ola Stenberg <hello@olastenberg.com>
2022-07-27 07:28:06 +0200

    refactor(subgraphs/sushiswap): change swap entity, use amountIn AmountOut, tokenIn, tokenOut

--- subgraphs/sushiswap/src/swap.ts
+++ subgraphs/sushiswap/src/swap.ts
@@ -1,6 +1,7 @@
 import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { Swap } from '../generated/schema'
 import { Swap as SwapEvent } from '../generated/templates/Pair/Pair'
+import { BIG_INT_ZERO } from './constants'
 import {
   convertTokenToDecimal, getOrCreateToken,
   getOrCreateTransaction,
@@ -15,23 +16,21 @@ export function handleSwap(event: SwapEvent, volumeUSD: BigDecimal): Swap {
   const swaps = transaction.swaps
 
   const pair = getPair(event.address.toHex())
-
-  const token0 = getOrCreateToken(pair.token0)
-  const token1 = getOrCreateToken(pair.token1)
-  const amount0In = convertTokenToDecimal(event.params.amount0In, token0.decimals)
-  const amount1In = convertTokenToDecimal(event.params.amount1In, token1.decimals)
-  const amount0Out = convertTokenToDecimal(event.params.amount0Out, token0.decimals)
-  const amount1Out = convertTokenToDecimal(event.params.amount1Out, token1.decimals)
+  const isFirstToken = event.params.amount0In.gt(BIG_INT_ZERO)
+  const tokenIn = getOrCreateToken(isFirstToken ? pair.token0 : pair.token1)
+  const tokenOut = getOrCreateToken(!isFirstToken ? pair.token0 : pair.token1)
+  const amountIn = convertTokenToDecimal(isFirstToken ? event.params.amount0In : event.params.amount1In, tokenIn.decimals)
+  const amountOut = convertTokenToDecimal(!isFirstToken ? event.params.amount0Out : event.params.amount1Out, tokenOut.decimals)
 
   const swap = new Swap(event.transaction.hash.toHex().concat('-').concat(BigInt.fromI32(swaps.length).toString()))
   swap.pair = pair.id
   swap.timestamp = transaction.createdAtTimestamp
   swap.transaction = transaction.id
   swap.sender = event.params.sender.toHex()
-  swap.amount0In = amount0In
-  swap.amount1In = amount1In
-  swap.amount0Out = amount0Out
-  swap.amount1Out = amount1Out
+  swap.tokenIn = tokenIn.id
+  swap.tokenOut = tokenOut.id
+  swap.amountIn = amountIn
+  swap.amountOut = amountOut
   swap.to = event.params.to.toHex()
   swap.logIndex = event.logIndex
   swap.amountUSD = volumeUSD
