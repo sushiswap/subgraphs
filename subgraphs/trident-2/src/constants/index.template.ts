9872b1b2ced1967573c029e5300ad8ea8320e30c
Ola Stenberg <hello@olastenberg.com>
2022-08-12 15:37:38 +0200

    fix(subgraphs/trident-2): template missing trident prefix

--- subgraphs/trident-2/src/constants/index.template.ts
+++ subgraphs/trident-2/src/constants/index.template.ts
@@ -70,7 +70,7 @@ export const PRESET_STABLE_POOL_ADDRESSES: string[] = '{{ trident.stablePoolAddr
 
 const STABLE_POOL_PERMUTATIONS = combinate(STABLE_TOKEN_ADDRESSES, NATIVE_ADDRESS, [false, true], [1, 5, 10, 30, 100])
 
-export const INIT_CODE_HASH = '{{ constantProductPoolFactory.initCodeHash }}'
+export const INIT_CODE_HASH = '{{ trident.constantProductPoolFactory.initCodeHash }}'
 
 export const STABLE_POOL_ADDRESSES: string[] = STABLE_POOL_PERMUTATIONS.map<string>((perm: CombinateReturn) => {
       const factory = Bytes.fromByteArray(Bytes.fromHexString('{{ constantProductPoolFactory.address }}'))
