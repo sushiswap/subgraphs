7272c41bcf96d57e2f866162a556d83f3ff69e33
Ola Stenberg <hello@olastenberg.com>
2022-08-12 16:30:03 +0200

    fix(subgraphs/trident-2): template missing trident prefix

--- subgraphs/trident-2/src/constants/index.template.ts
+++ subgraphs/trident-2/src/constants/index.template.ts
@@ -48,16 +48,16 @@ export const ADDRESS_ZERO = Address.fromString('0x000000000000000000000000000000
 
 export const BENTOBOX_ADDRESS = Address.fromString('{{ bentobox.address }}')
 
-export const MASTER_DEPLOYER_ADDRESS = Address.fromString('{{ masterDeployer.address }}')
+export const MASTER_DEPLOYER_ADDRESS = Address.fromString('{{ trident.masterDeployer.address }}')
 
-export const CONSTANT_PRODUCT_POOL_FACTORY_ADDRESS = Address.fromString('{{ constantProductPoolFactory.address }}')
+export const CONSTANT_PRODUCT_POOL_FACTORY_ADDRESS = Address.fromString('{{ trident.constantProductPoolFactory.address }}')
 
-export const HYBRID_POOL_FACTORY_ADDRESS = Address.fromString('{{ hybridPoolFactory.address }}')
+export const HYBRID_POOL_FACTORY_ADDRESS = Address.fromString('{{ trident.hybridPoolFactory.address }}')
 
-export const INDEX_POOL_FACTORY_ADDRESS = Address.fromString('{{ indexPoolFactory.address }}')
+export const INDEX_POOL_FACTORY_ADDRESS = Address.fromString('{{ trident.indexPoolFactory.address }}')
 
 export const CONCENTRATED_LIQUIDITY_POOL_FACTORY_ADDRESS = Address.fromString(
-  '{{ concentratedLiquidityPoolFactory.address }}'
+  '{{ trident.concentratedLiquidityPoolFactory.address }}'
 )
 
 export const NATIVE_ADDRESS = '{{ trident.native.address }}'
