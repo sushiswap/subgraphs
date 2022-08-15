6f6a28b849ac8cae8782818ca1facb9f1b57bfdd
Ola Stenberg <hello@olastenberg.com>
2022-08-15 20:21:34 +0200

    feat(subgraphs/trident-2): enable cpp

--- subgraphs/trident-2/src/mappings/master-deployer.ts
+++ subgraphs/trident-2/src/mappings/master-deployer.ts
@@ -9,9 +9,8 @@ import { createPair } from '../functions/pair'
 export function onDeployPool(event: DeployPool): void {
 
   if (event.params.factory == CONSTANT_PRODUCT_POOL_FACTORY_ADDRESS) {
-    // FIXME: enable these later, just disabled for tesing stable pools atm
-    // createPair(event, PairType.CONSTANT_PRODUCT_POOL)
-    // ConstantProductPool.create(event.params.pool)
+    createPair(event, PairType.CONSTANT_PRODUCT_POOL)
+    ConstantProductPool.create(event.params.pool)
   } else if (event.params.factory == STABLE_POOL_FACTORY_ADDRESS) {
     createPair(event, PairType.STABLE_POOL)
     StablePool.create(event.params.pool)
