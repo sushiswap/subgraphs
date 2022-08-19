01e07820054095c84dc3e54e7778b5f89c9945b9
Ola Stenberg <hello@olastenberg.com>
2022-08-19 13:13:41 +0200

    feat(subgraphs/user): init

---
+++ subgraphs/user/src/constants/index.template.ts
@@ -0,0 +1,18 @@
+import { Address, BigDecimal, BigInt, ByteArray, Bytes, crypto, ethereum } from '@graphprotocol/graph-ts'
+export * from './time'
+
+
+
+export const NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
+
+export const BIG_INT_ZERO = BigInt.fromI32(0)
+
+export const BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
+
+export const BIG_DECIMAL_ONE = BigDecimal.fromString('1')
+
+export const BIG_INT_ONE = BigInt.fromI32(1)
+
+export const ADDRESS_ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
+
+
