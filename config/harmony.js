091a7cb05fc4ccd07abe60406514d09f49b65b78
Ola Stenberg <hello@olastenberg.com>
2022-11-17 12:51:26 +0100

    fix: harmony

--- config/harmony.js
+++ config/harmony.js
@@ -1,11 +1,14 @@
 const NATIVE_ADDRESS = '0xcf664087a5bb0237a0bad6742852ec6c8d69a27a'
 const WETH_ADDRESS = '0x6983d1e6def3690c4d616b13597a09e6193ea013'
 const WBTC_ADDRESS = '0x3095c7557bcb296ccc6e363de01b760ba031f2d9'
-const USDC_ADDRESS = '0x985458e523db3d53125813ed68c274899e9dfab4'
-const USDT_ADDRESS = '0x3c2b8be99c50593081eaa2a724f0b8285f5aba8f'
+// const USDC_ADDRESS = '0x985458e523db3d53125813ed68c274899e9dfab4'
+// const USDT_ADDRESS = '0x3c2b8be99c50593081eaa2a724f0b8285f5aba8f'
 const BUSD_ADDRESS = '0xe176ebe47d621b984a73036b9da5d834411ef734'
-const DAI_ADDRESS = '0xef977d2f931c1978db5f6747666fa1eacb0d0339'
-const FRAX_ADDRESS = '0xfa7191d292d5633f702b0bd7e3e3bccc0e633200'
+const USDS_ADDRESS = '0x471f66f75af9238a2fa23ba23862b5957109fb21'
+// const DAI_ADDRESS = '0xef977d2f931c1978db5f6747666fa1eacb0d0339'
+// const FRAX_ADDRESS = '0xfa7191d292d5633f702b0bd7e3e3bccc0e633200'
+// After block 28104617, all
+
 
 module.exports = {
   network: 'harmony',
@@ -31,13 +34,18 @@ module.exports = {
       NATIVE_ADDRESS,
       WETH_ADDRESS,
       WBTC_ADDRESS,
-      USDC_ADDRESS,
-      USDT_ADDRESS,
+      // USDC_ADDRESS,
+      // USDT_ADDRESS,
       BUSD_ADDRESS,
-      DAI_ADDRESS,
-      FRAX_ADDRESS
+      USDS_ADDRESS,
+      // DAI_ADDRESS,
+      // FRAX_ADDRESS
     ],
-    stableTokenAddresses: [USDC_ADDRESS, USDT_ADDRESS, BUSD_ADDRESS, DAI_ADDRESS, FRAX_ADDRESS],
+    stableTokenAddresses: [
+      // USDC_ADDRESS, USDT_ADDRESS, BUSD_ADDRESS, DAI_ADDRESS, FRAX_ADDRESS
+      BUSD_ADDRESS,
+      USDS_ADDRESS,
+    ],
     minimumNativeLiquidity: 50000,
     minimum_usd_threshold_new_pairs: '1000',
     factory: {
