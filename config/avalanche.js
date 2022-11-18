648dbb24cf571e16d7a5fbfee89a9a497f7c1daa
Ola Stenberg <hello@olastenberg.com>
2022-11-18 14:01:04 +0100

    chore: add avax and bsc trident config

--- config/avalanche.js
+++ config/avalanche.js
@@ -47,6 +47,24 @@ module.exports = {
       startBlock: 506190,
     },
   },
+  trident: {
+    masterDeployer: { address: '0x97a32b4f8486735075f2cbecff64208fbf2e610a', startBlock: 22495996 },
+    concentratedLiquidityPoolFactory: { address: '0x0000000000000000000000000000000000000000' },
+    constantProductPoolFactory: {
+      address: '0xb84a043bc4fca97b7a74ed7daab1bf12a8df929f',
+      initCodeHash: '0x3172d82413be467c1130709f7479a07def9b99caf8e0059f248c131081e4ea09',
+    },
+    stablePoolFactory: { address: '0x7770978eed668a3ba661d51a773d3a992fc9ddcb' },
+    hybridPoolFactory: { address: '0x0000000000000000000000000000000000000000' },
+    indexPoolFactory: { address: '0x0000000000000000000000000000000000000000' },
+    native: { address: NATIVE_ADDRESS },
+    stableTokenAddresses: [USDC_ADDRESS, USDT_ADDRESS, USDTE_ADDRESS, DAI_ADDRESS, MIM_ADDRESS],
+    tokensToPriceOffNative: [
+      // These tokens will be priced off the NATIVE token.
+      USDC_ADDRESS,
+    ],
+    minimumNativeLiquidity: '75',
+  },
   furo: {
     stream: { address: '0x4ab2fc6e258a0ca7175d05ff10c5cf798a672cae', startBlock: 15714979 },
     vesting: { address: '0x0689640d190b10765f09310fcfe9c670ede4e25b', startBlock: 15715037 },
@@ -64,5 +82,5 @@ module.exports = {
   stargate: {
     usdcPool: { address: '0x1205f31718499dbf1fca446663b532ef87481fe1', startBlock: 12219159 },
     usdtPool: { address: '0x29e38769f23701a2e4a8ef0492e19da4604be62c', startBlock: 12219171 },
-  }
+  },
 }
