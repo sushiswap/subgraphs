661b9d4c39205bf609cc98ccbd5ea38afdf531c5
Ola Stenberg <hello@olastenberg.com>
2023-04-06 15:04:21 +0200

    feat(config/polygon-zkevm): prepare config

---
+++ config/polygon-zkevm.js
@@ -0,0 +1,38 @@
+const NATIVE_ADDRESS = '0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9'
+const WETH_ADDRESS = '0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9'
+const WBTC_ADDRESS = '0xea034fb02eb1808c2cc3adbc15f447b93cbe08e1'
+const USDC_ADDRESS = '0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035'
+const USDT_ADDRESS = '0x1e4a5963abfd975d8c9021ce480b42188849d41d'
+const DAI_ADDRESS = '0xc5015b9d9161dca7e18e32f6f25c4ad850731fd4'
+const MATIC_ADDRESS = '0xa2036f0538221a77a3937f1379699f44945018d0'
+
+module.exports = {
+  network: 'polygon-zkevm',
+  // sushi: { address: SUSHI_ADDRESS },
+  weth: { address: WETH_ADDRESS },
+  wbtc: { address: WBTC_ADDRESS },
+
+  v3: {
+    factory: {
+      address: '0x1b02da8cb0d097eb8d57a175b88c7d8b47997506',
+      startBlock: 80860,
+    },
+    positionManager: {
+      address: '0xf4d73326c13a4fc5fd7a064217e12780e9bd62c3',
+      startBlock: 81001,
+    },
+    native: { address: NATIVE_ADDRESS },
+    whitelistedTokenAddresses: [
+      // IMPORTANT! Native should be included here
+      NATIVE_ADDRESS,
+      WBTC_ADDRESS,
+      USDC_ADDRESS,
+      USDT_ADDRESS,
+      DAI_ADDRESS,
+      MATIC_ADDRESS,
+    ],
+    stableTokenAddresses: [USDC_ADDRESS, USDT_ADDRESS, DAI_ADDRESS],
+    nativePricePool: '', // TODO: add this
+    minimumEthLocked: 0.7,
+  },
+}
