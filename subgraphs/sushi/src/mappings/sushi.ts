b5f9fdf53865f156cba5eb639141dc815492bff6
HHK <hhk.contact@protonmail.com>
2023-05-11 15:57:59 -0500

    fix: index addr 0 but not balance change

--- subgraphs/sushi/src/mappings/sushi.ts
+++ subgraphs/sushi/src/mappings/sushi.ts
@@ -5,25 +5,25 @@ import { getOrCreateTransaction } from '../functions/transaction'
 import { getOrCreateUser } from '../functions/user'
 
 export function onTransfer(event: TransferEvent): void {
-  getOrCreateTransaction(event)
-
-  //we don't update balance of address(0) when minting
   const senderAddress = event.params.from
+  let sender = getOrCreateUser(senderAddress.toHex(), event)
+  sender.modifiedAtBlock = event.block.number
+  sender.modifiedAtTimestamp = event.block.timestamp
+  //we don't update balance of address(0) when minting
   if (senderAddress != ADDRESS_ZERO) {
-    let sender = getOrCreateUser(senderAddress.toHex(), event)
     sender.balance = sender.balance.minus(event.params.value)
-    sender.modifiedAtBlock = event.block.number
-    sender.modifiedAtTimestamp = event.block.timestamp
-    sender.save()
   }
+  sender.save()
 
-  //we don't update balance of address(0) when burning
   const receiverAddress = event.params.to
+  let receiver = getOrCreateUser(receiverAddress.toHex(), event)
+  receiver.modifiedAtBlock = event.block.number
+  receiver.modifiedAtTimestamp = event.block.timestamp
+  //we don't update balance of address(0) when burning
   if (receiverAddress != ADDRESS_ZERO) {
-    let receiver = getOrCreateUser(receiverAddress.toHex(), event)
     receiver.balance = receiver.balance.plus(event.params.value)
-    receiver.modifiedAtBlock = event.block.number
-    receiver.modifiedAtTimestamp = event.block.timestamp
-    receiver.save()
   }
+  receiver.save()
+
+  getOrCreateTransaction(event)
 }
--- subgraphs/sushi/tests/Sushi.test.ts
+++ subgraphs/sushi/tests/Sushi.test.ts
@@ -4,7 +4,7 @@ import { ADDRESS_ZERO, SUSHI } from '../src/constants'
 import { onTransfer } from '../src/mappings/sushi'
 import { createTransferEvent } from './mocks'
 
-test('Sushi entitys count field increase on transactions', () => {
+test('Sushi entities count field increase on transactions', () => {
   const alice = Address.fromString('0x00000000000000000000000000000000000a71ce')
   const bob = Address.fromString('0x0000000000000000000000000000000000000b0b')
   const amount = BigInt.fromString('1337')
@@ -25,9 +25,9 @@ test('Sushi entitys count field increase on transactions', () => {
   mintEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000001') as Bytes
   onTransfer(mintEvent)
 
-  // Then: transaction count increased and userCount remains
+  // Then: transaction count increased
   assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '2')
-  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
+  assert.fieldEquals('Sushi', SUSHI, 'userCount', '3')
 
   // And: supply is increased
   assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
@@ -39,7 +39,7 @@ test('Sushi entitys count field increase on transactions', () => {
 
   // Then: the transaction count increase and userCount remains
   assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '3')
-  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
+  assert.fieldEquals('Sushi', SUSHI, 'userCount', '3')
 
   // And: the total supply is decreased
   assert.fieldEquals('Sushi', SUSHI, 'totalSupply', '1000')
