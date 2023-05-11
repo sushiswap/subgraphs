be8f5a777022d14a6dca0f8768d7f4c3f79a7c01
HHK <hhk.contact@protonmail.com>
2023-05-11 09:49:32 -0500

    fix: fix tests

--- subgraphs/sushi/tests/Sushi.test.ts
+++ subgraphs/sushi/tests/Sushi.test.ts
@@ -1,46 +1,46 @@
 import { Address, BigInt, Bytes } from '@graphprotocol/graph-ts'
-import { assert, test, logStore} from 'matchstick-as/assembly/index'
-import { ADDRESS_ZERO, SUSHI } from '../src/mappings/constants'
+import { assert, test, logStore } from 'matchstick-as/assembly/index'
+import { ADDRESS_ZERO, SUSHI } from '../src/constants'
 import { onTransfer } from '../src/mappings/sushi'
 import { createTransferEvent } from './mocks'
 
 test('Sushi entitys count field increase on transactions', () => {
-    const alice = Address.fromString('0x00000000000000000000000000000000000a71ce')
-    const bob = Address.fromString('0x0000000000000000000000000000000000000b0b')
-    const amount = BigInt.fromString("1337")
-    let transferEvent = createTransferEvent(alice, bob, amount)
+  const alice = Address.fromString('0x00000000000000000000000000000000000a71ce')
+  const bob = Address.fromString('0x0000000000000000000000000000000000000b0b')
+  const amount = BigInt.fromString('1337')
+  let transferEvent = createTransferEvent(alice, bob, amount)
 
-    // When: Alice transfers to Bob
-    onTransfer(transferEvent)
+  // When: Alice transfers to Bob
+  onTransfer(transferEvent)
 
-    // Then: The sushi entitys fields are updated
-    assert.fieldEquals('Sushi', SUSHI, 'transactionCount', "1")
-    assert.fieldEquals('Sushi', SUSHI, 'userCount',  "2")
+  // Then: The sushi entitys fields are updated
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '1')
+  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
 
-    // And: supply remains unchanged
-    assert.fieldEquals('Sushi', SUSHI, 'totalSupply',  "0")
+  // And: supply remains unchanged
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', '0')
 
-    // When: the zero-address make a transaction (mint)
-    let mintEvent = createTransferEvent(ADDRESS_ZERO, bob, amount)
-    mintEvent.transaction.hash = Address.fromString("0x0000000000000000000000000000000000000001") as Bytes;
-    onTransfer(mintEvent)
+  // When: the zero-address make a transaction (mint)
+  let mintEvent = createTransferEvent(ADDRESS_ZERO, bob, amount)
+  mintEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000001') as Bytes
+  onTransfer(mintEvent)
 
-    // Then: count fields are increased
-    assert.fieldEquals('Sushi', SUSHI, 'transactionCount', "2")
-    assert.fieldEquals('Sushi', SUSHI, 'userCount',  "3")
+  // Then: transaction count increased and userCount remains
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '2')
+  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
 
-    // And: supply is increased
-    assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
+  // And: supply is increased
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
 
-    // When: the zero-address recieves a transaction (burn)
-    let burnEvent = createTransferEvent(alice, ADDRESS_ZERO, BigInt.fromString("337"))
-    burnEvent.transaction.hash = Address.fromString("0x0000000000000000000000000000000000000002") as Bytes;
-    onTransfer(burnEvent)
+  // When: the zero-address receives a transaction (burn)
+  let burnEvent = createTransferEvent(alice, ADDRESS_ZERO, BigInt.fromString('337'))
+  burnEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000002') as Bytes
+  onTransfer(burnEvent)
 
-    // Then: the transaction count increase and userCount remains
-    assert.fieldEquals('Sushi', SUSHI, 'transactionCount', "3")
-    assert.fieldEquals('Sushi', SUSHI, 'userCount',  "3")
+  // Then: the transaction count increase and userCount remains
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '3')
+  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
 
-    // And: the total supply is decreased
-    assert.fieldEquals('Sushi', SUSHI, 'totalSupply', "1000")
+  // And: the total supply is decreased
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', '1000')
 })
--- subgraphs/sushi/tests/Transaction.test.ts
+++ subgraphs/sushi/tests/Transaction.test.ts
@@ -1,6 +1,6 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { assert, test, clearStore } from 'matchstick-as/assembly/index'
-import { ADDRESS_ZERO, BURN, MINT, TRANSFER } from '../src/mappings/constants'
+import { ADDRESS_ZERO, BURN, MINT, TRANSFER } from '../src/constants'
 import { onTransfer } from '../src/mappings/sushi'
 import { createTransferEvent } from './mocks'
 
@@ -10,15 +10,15 @@ function cleanup(): void {
 
 test('Transfer', () => {
   const sender = Address.fromString('0x00000000000000000000000000000000000a71ce')
-  const reciever = Address.fromString('0x0000000000000000000000000000000000000b0b')
+  const receiver = Address.fromString('0x0000000000000000000000000000000000000b0b')
   const amount = BigInt.fromString('1337')
-  let transferEvent = createTransferEvent(sender, reciever, amount)
+  let transferEvent = createTransferEvent(sender, receiver, amount)
 
   onTransfer(transferEvent)
-  const transactionId = transferEvent.transaction.hash.toHex()
+  const transactionId = transferEvent.transaction.hash.toHex() + '-' + transferEvent.logIndex.toHex()
   assert.fieldEquals('Transaction', transactionId, 'id', transactionId)
   assert.fieldEquals('Transaction', transactionId, 'from', sender.toHex())
-  assert.fieldEquals('Transaction', transactionId, 'to', reciever.toHex())
+  assert.fieldEquals('Transaction', transactionId, 'to', receiver.toHex())
   assert.fieldEquals('Transaction', transactionId, 'amount', transferEvent.params.value.toString())
   assert.fieldEquals('Transaction', transactionId, 'gasUsed', transferEvent.block.gasUsed.toString())
   assert.fieldEquals('Transaction', transactionId, 'type', TRANSFER)
@@ -31,25 +31,25 @@ test('Transfer', () => {
 })
 
 test('Zero address is sender sets transaction type to MINT', () => {
-  const reciever = Address.fromString('0x0000000000000000000000000000000000000b0b')
+  const receiver = Address.fromString('0x0000000000000000000000000000000000000b0b')
   const amount = BigInt.fromString('1337')
-  let transferEvent = createTransferEvent(ADDRESS_ZERO, reciever, amount)
+  let transferEvent = createTransferEvent(ADDRESS_ZERO, receiver, amount)
 
   onTransfer(transferEvent)
-  const transactionId = transferEvent.transaction.hash.toHex()
+  const transactionId = transferEvent.transaction.hash.toHex() + '-' + transferEvent.logIndex.toHex()
   assert.fieldEquals('Transaction', transactionId, 'from', ADDRESS_ZERO.toHex())
   assert.fieldEquals('Transaction', transactionId, 'type', MINT)
 
   cleanup()
 })
 
-test('Zero address is reciever sets transaction type to BURN', () => {
+test('Zero address is receiver sets transaction type to BURN', () => {
   const sender = Address.fromString('0x0000000000000000000000000000000000000b0b')
   const amount = BigInt.fromString('1337')
   let transferEvent = createTransferEvent(sender, ADDRESS_ZERO, amount)
 
   onTransfer(transferEvent)
-  const transactionId = transferEvent.transaction.hash.toHex()
+  const transactionId = transferEvent.transaction.hash.toHex() + '-' + transferEvent.logIndex.toHex()
   assert.fieldEquals('Transaction', transactionId, 'to', ADDRESS_ZERO.toHex())
   assert.fieldEquals('Transaction', transactionId, 'type', BURN)
 
