ea8ac236d4c97d49366b9994c66cde4635d589ae
HHK <hhk.contact@protonmail.com>
2023-05-12 09:52:42 -0500

    feat: add holderCount to test and add more checks

--- subgraphs/sushi/tests/Sushi.test.ts
+++ subgraphs/sushi/tests/Sushi.test.ts
@@ -8,39 +8,58 @@ test('Sushi entities count field increase on transactions', () => {
   const alice = Address.fromString('0x00000000000000000000000000000000000a71ce')
   const bob = Address.fromString('0x0000000000000000000000000000000000000b0b')
   const amount = BigInt.fromString('1337')
-  let transferEvent = createTransferEvent(alice, bob, amount)
-
-  // When: Alice transfers to Bob
-  onTransfer(transferEvent)
-
-  // Then: The sushi entitys fields are updated
-  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '1')
-  assert.fieldEquals('Sushi', SUSHI, 'userCount', '2')
-
-  // And: supply remains unchanged
-  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', '0')
 
   // When: the zero-address make a transaction (mint)
   let mintEvent = createTransferEvent(ADDRESS_ZERO, bob, amount)
   mintEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000001') as Bytes
   onTransfer(mintEvent)
 
-  // Then: transaction count increased
-  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '2')
-  assert.fieldEquals('Sushi', SUSHI, 'userCount', '3')
+  // Then: The sushi entities fields are updated
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '1')
+  assert.fieldEquals('Sushi', SUSHI, 'totalUserCount', '2')
+  assert.fieldEquals('Sushi', SUSHI, 'holderCount', '1')
 
-  // And: supply is increased
+  // And: supply remains changed
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
+
+  let transferEvent = createTransferEvent(bob, alice, amount)
+  transferEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000002') as Bytes
+
+  // When: Bob transfers all sushi to Alice
+  onTransfer(transferEvent)
+
+  // Then: transaction count & userCount increased but holderCount unchanged as lost 1 holder and gain 1 holder
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '2')
+  assert.fieldEquals('Sushi', SUSHI, 'totalUserCount', '3')
+  assert.fieldEquals('Sushi', SUSHI, 'holderCount', '1')
+
+  // And: supply is unchanged
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
+
+  // When: Alice transfer half back to Bob
+  transferEvent = createTransferEvent(alice, bob, amount.div(BigInt.fromU32(2)))
+  transferEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000003') as Bytes
+  onTransfer(transferEvent)
+
+  // Then: transaction count & holderCount increased but totalUserCount unchanged as no new user
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '3')
+  assert.fieldEquals('Sushi', SUSHI, 'totalUserCount', '3')
+  assert.fieldEquals('Sushi', SUSHI, 'holderCount', '2')
+
+  // And: supply is unchanged
   assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.toString())
 
   // When: the zero-address receives a transaction (burn)
-  let burnEvent = createTransferEvent(alice, ADDRESS_ZERO, BigInt.fromString('337'))
-  burnEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000002') as Bytes
+  let burnEvent = createTransferEvent(bob, ADDRESS_ZERO, amount.div(BigInt.fromU32(2)))
+  burnEvent.transaction.hash = Address.fromString('0x0000000000000000000000000000000000000004') as Bytes
   onTransfer(burnEvent)
 
-  // Then: the transaction count increase and userCount remains
-  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '3')
-  assert.fieldEquals('Sushi', SUSHI, 'userCount', '3')
+  // Then: the transaction count increase and totalUserCount remains but holderCount reduce as lost one holder (alice)
+  assert.fieldEquals('Sushi', SUSHI, 'transactionCount', '4')
+  assert.fieldEquals('Sushi', SUSHI, 'totalUserCount', '3')
+  assert.fieldEquals('User', bob.toHex(), 'balance', BigInt.zero().toString())
+  assert.fieldEquals('Sushi', SUSHI, 'holderCount', '1')
 
   // And: the total supply is decreased
-  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', '1000')
+  assert.fieldEquals('Sushi', SUSHI, 'totalSupply', amount.minus(amount.div(BigInt.fromU32(2))).toString())
 })
