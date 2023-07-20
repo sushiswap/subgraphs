bf341facfcf96caf6731b8b2d1eb5ca42e9bd670
Ola Stenberg <hello@olastenberg.com>
2023-07-20 12:03:54 +0200

    chore(config/core): change wnative and price pool

--- config/core.js
+++ config/core.js
@@ -1,5 +1,5 @@
-const NATIVE_ADDRESS = '0x191e94fa59739e188dce837f7f6978d84727ad01'
-const NATIVE2_ADDRESS = '0x40375c92d9faf44d2f9db9bd9ba41a3317a2404f'
+const NATIVE_ADDRESS = '0x40375c92d9faf44d2f9db9bd9ba41a3317a2404f'
+const NATIVE2_ADDRESS = '0x191e94fa59739e188dce837f7f6978d84727ad01'
 const USDT_ADDRESS = '0x900101d06a7426441ae63e9ab3b9b0f63be145f1'
 const USDC_ADDRESS = '0xa4151b2b3e269645181dccf2d426ce75fcbdeca9'
 const WETH_ADDRESS = '0xeab3ac417c4d6df6b143346a46fee1b847b50296'
@@ -29,7 +29,7 @@ module.exports = {
       WETH_ADDRESS,
     ],
     stableTokenAddresses: [USDT_ADDRESS, USDC_ADDRESS],
-    nativePricePool: '0x54ebeed7274e3484bf84b8cf5520a3ff547f584c', // WCORE/USDT
+    nativePricePool: '0x72d8e6d7307dcf11a3456b9f6fdfad05385b2f3e', // WCORE/USDT
     minimumEthLocked: 1000,
   },
 }
