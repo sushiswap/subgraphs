7f753b5edcd3b414fd5e432236706d9b1ec72060
OlaStenberg <hello@olastenberg.com>
2024-12-24 18:56:58 +0100

    feat: disable NftPosManager.positions call

--- subgraphs/v3/src/mappings/position-manager.ts
+++ subgraphs/v3/src/mappings/position-manager.ts
@@ -13,55 +13,55 @@ import { convertTokenToDecimal, loadTransaction } from '../utils'
 
 function getPosition(event: ethereum.Event, tokenId: BigInt): Position | null {
   let position = Position.load(tokenId.toString())
-  if (position === null) {
-    let contract = NonfungiblePositionManager.bind(event.address)
-    let positionCall = contract.try_positions(tokenId)
+  // if (position === null) {
+  //   let contract = NonfungiblePositionManager.bind(event.address)
+  //   let positionCall = contract.try_positions(tokenId)
 
-    // the following call reverts in situations where the position is minted
-    // and deleted in the same block - from my investigation this happens
-    // in calls from  BancorSwap
-    // (e.g. 0xf7867fa19aa65298fadb8d4f72d0daed5e836f3ba01f0b9b9631cdc6c36bed40)
-    if (!positionCall.reverted) {
-      let positionResult = positionCall.value
-      let poolAddress = factoryContract.getPool(positionResult.value2, positionResult.value3, positionResult.value4)
+  //   // the following call reverts in situations where the position is minted
+  //   // and deleted in the same block - from my investigation this happens
+  //   // in calls from  BancorSwap
+  //   // (e.g. 0xf7867fa19aa65298fadb8d4f72d0daed5e836f3ba01f0b9b9631cdc6c36bed40)
+  //   if (!positionCall.reverted) {
+  //     let positionResult = positionCall.value
+  //     let poolAddress = factoryContract.getPool(positionResult.value2, positionResult.value3, positionResult.value4)
 
-      position = new Position(tokenId.toString())
-      // The owner gets correctly updated in the Transfer handler
-      position.owner = Address.fromString(ADDRESS_ZERO)
-      position.pool = poolAddress.toHexString()
-      position.token0 = positionResult.value2.toHexString()
-      position.token1 = positionResult.value3.toHexString()
-      position.tickLower = position.pool.concat('#').concat(positionResult.value5.toString())
-      position.tickUpper = position.pool.concat('#').concat(positionResult.value6.toString())
-      position.liquidity = ZERO_BI
-      position.depositedToken0 = ZERO_BD
-      position.depositedToken1 = ZERO_BD
-      position.withdrawnToken0 = ZERO_BD
-      position.withdrawnToken1 = ZERO_BD
-      position.collectedToken0 = ZERO_BD
-      position.collectedToken1 = ZERO_BD
-      position.collectedFeesToken0 = ZERO_BD
-      position.collectedFeesToken1 = ZERO_BD
-      position.transaction = loadTransaction(event).id
-      position.feeGrowthInside0LastX128 = positionResult.value8
-      position.feeGrowthInside1LastX128 = positionResult.value9
+  //     position = new Position(tokenId.toString())
+  //     // The owner gets correctly updated in the Transfer handler
+  //     position.owner = Address.fromString(ADDRESS_ZERO)
+  //     position.pool = poolAddress.toHexString()
+  //     position.token0 = positionResult.value2.toHexString()
+  //     position.token1 = positionResult.value3.toHexString()
+  //     position.tickLower = position.pool.concat('#').concat(positionResult.value5.toString())
+  //     position.tickUpper = position.pool.concat('#').concat(positionResult.value6.toString())
+  //     position.liquidity = ZERO_BI
+  //     position.depositedToken0 = ZERO_BD
+  //     position.depositedToken1 = ZERO_BD
+  //     position.withdrawnToken0 = ZERO_BD
+  //     position.withdrawnToken1 = ZERO_BD
+  //     position.collectedToken0 = ZERO_BD
+  //     position.collectedToken1 = ZERO_BD
+  //     position.collectedFeesToken0 = ZERO_BD
+  //     position.collectedFeesToken1 = ZERO_BD
+  //     position.transaction = loadTransaction(event).id
+  //     position.feeGrowthInside0LastX128 = positionResult.value8
+  //     position.feeGrowthInside1LastX128 = positionResult.value9
 
-      position.amountDepositedUSD = ZERO_BD
-      position.amountWithdrawnUSD = ZERO_BD
-      position.amountCollectedUSD = ZERO_BD
-    }
-  }
+  //     position.amountDepositedUSD = ZERO_BD
+  //     position.amountWithdrawnUSD = ZERO_BD
+  //     position.amountCollectedUSD = ZERO_BD
+  //   }
+  // }
 
   return position
 }
 
 function updateFeeVars(position: Position, event: ethereum.Event, tokenId: BigInt): Position {
-  let positionManagerContract = NonfungiblePositionManager.bind(event.address)
-  let positionResult = positionManagerContract.try_positions(tokenId)
-  if (!positionResult.reverted) {
-    position.feeGrowthInside0LastX128 = positionResult.value.value8
-    position.feeGrowthInside1LastX128 = positionResult.value.value9
-  }
+  // let positionManagerContract = NonfungiblePositionManager.bind(event.address)
+  // let positionResult = positionManagerContract.try_positions(tokenId)
+  // if (!positionResult.reverted) {
+  //   position.feeGrowthInside0LastX128 = positionResult.value.value8
+  //   position.feeGrowthInside1LastX128 = positionResult.value.value9
+  // }
   return position
 }
 
