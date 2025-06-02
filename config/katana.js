85c9b788caa7564e99e992fcefed967c24b82709
0xMasayoshi <0xMasayoshi@protonmail.com>
2025-06-02 23:07:31 +0700

    chore: update katana config

--- config/katana.js
+++ config/katana.js
@@ -1,5 +1,8 @@
 const NATIVE_ADDRESS = '0xee7d8bcfb72bc1880d0cf19822eb0a2e6577ab62'
-const USDC_ADDRESS = '0x' // TODO
+const vbUSDC_ADDRESS = '0x203a662b0bd271a6ed5a60edfbd04bfce608fd36'
+const vbUSDT_ADDRESS = '0x2dca96907fde857dd3d816880a0df407eeb2d2f2'
+const vbUSDS_ADDRESS = '0x62d6a123e8d19d06d68cf0d2294f9a3a0362c6b3'
+const vbWBTC_ADDRESS = '0x0913da6da4b42f538b445599b46bb4622342cf52'
 
 module.exports = {
   network: 'katana',
@@ -12,11 +15,14 @@ module.exports = {
     whitelistAddresses: [
       // IMPORTANT! Native should be included here
       NATIVE_ADDRESS,
-      USDC_ADDRESS,
+      vbUSDC_ADDRESS,
+      vbUSDT_ADDRESS,
+      vbUSDS_ADDRESS,
+      vbWBTC_ADDRESS
     ],
-    stable0: USDC_ADDRESS,
-    // stable1: TODO,
-    // stable2: TODO,
+    stable0: vbUSDC_ADDRESS,
+    stable1: vbUSDT_ADDRESS,
+    stable2: vbUSDS_ADDRESS,
     minimumNativeLiquidity: 0.5,
     factory: {
       address: '0x72d111b4d6f31b38919ae39779f570b747d6acd9',
@@ -37,12 +43,17 @@ module.exports = {
     whitelistedTokenAddresses: [
       // IMPORTANT! Native should be included here
       NATIVE_ADDRESS,
-      USDC_ADDRESS,
+      vbUSDC_ADDRESS,
+      vbUSDT_ADDRESS,
+      vbUSDS_ADDRESS,
+      vbWBTC_ADDRESS
     ],
     stableTokenAddresses: [
-      USDC_ADDRESS
+      vbUSDC_ADDRESS,
+      vbUSDT_ADDRESS,
+      vbUSDS_ADDRESS,
     ],
-    nativePricePool: 'TODO', // WETH/USDC - 0.03%
+    nativePricePool: '0x105f833d8522f33d8dc3e9599455e9412b63d049', // WETH/USDC - 0.03%
     minimumEthLocked: 0.5,
   },
 }
